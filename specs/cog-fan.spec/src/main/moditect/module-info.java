329ce588e339775f4880cddd5cba456f874805ce
John Fallows <john.r.fallows@gmail.com>
2021-12-15 09:25:12 -0800

    fan cog spec

--- specs/cog-fan.spec/src/main/moditect/module-info.java
+++ specs/cog-fan.spec/src/main/moditect/module-info.java
@@ -13,9 +13,7 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
-scope echo
+open module io.aklivity.zilla.specs.cog.fan
 {
-    scope stream
-    {
-    }
+    requires transitive io.aklivity.zilla.specs.cog;
 }
