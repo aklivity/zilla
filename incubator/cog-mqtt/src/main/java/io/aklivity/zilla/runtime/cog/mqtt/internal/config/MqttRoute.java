97dffb81385bde8317542169e6b4b1f30bec38cd
John Fallows <john.r.fallows@gmail.com>
2021-12-15 16:33:06 -0800

    MQTT cog implementation

---
+++ incubator/cog-mqtt/src/main/java/io/aklivity/zilla/runtime/cog/mqtt/internal/config/MqttRoute.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2021-2021 Aklivity Inc.
+ *
+ * Aklivity licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package io.aklivity.zilla.runtime.cog.mqtt.internal.config;
+
+import static java.util.stream.Collectors.toList;
+
+import java.util.List;
+
+import io.aklivity.zilla.runtime.engine.config.Options;
+import io.aklivity.zilla.runtime.engine.config.Route;
+
+public final class MqttRoute extends Options
+{
+    public final long id;
+    public final List<MqttMatcher> when;
+
+    public MqttRoute(
+        Route route)
+    {
+        this.id = route.id;
+        this.when = route.when.stream()
+            .map(MqttCondition.class::cast)
+            .map(MqttMatcher::new)
+            .collect(toList());
+    }
+}
