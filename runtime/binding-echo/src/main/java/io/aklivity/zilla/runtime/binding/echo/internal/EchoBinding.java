eb5eb2f3d918ca9e71254e4857af44db6932ccc0
John Fallows <john.r.fallows@gmail.com>
2022-01-13 16:05:44 -0800

    Refactor cog-echo to binding-echo

---
+++ runtime/binding-echo/src/main/java/io/aklivity/zilla/runtime/binding/echo/internal/EchoBinding.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2021-2022 Aklivity Inc.
+ *
+ * Aklivity licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package io.aklivity.zilla.runtime.binding.echo.internal;
+
+import java.net.URL;
+
+import io.aklivity.zilla.runtime.engine.EngineContext;
+import io.aklivity.zilla.runtime.engine.binding.Binding;
+
+final class EchoBinding implements Binding
+{
+    static final String NAME = "echo";
+
+    private final EchoConfiguration config;
+
+    EchoBinding(
+        EchoConfiguration config)
+    {
+        this.config = config;
+    }
+
+    @Override
+    public String name()
+    {
+        return EchoBinding.NAME;
+    }
+
+    @Override
+    public URL type()
+    {
+        return getClass().getResource("schema/echo.json");
+    }
+
+    @Override
+    public EchoBindingContext supply(
+        EngineContext context)
+    {
+        return new EchoBindingContext(config, context);
+    }
+}
---
+++ specs/binding-echo.spec/src/test/java/io/aklivity/zilla/specs/binding/echo/config/SchemaTest.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2021-2022 Aklivity Inc.
+ *
+ * Aklivity licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package io.aklivity.zilla.specs.binding.echo.config;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.nullValue;
+
+import jakarta.json.JsonObject;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import io.aklivity.zilla.specs.engine.config.ConfigSchemaRule;
+
+public class SchemaTest
+{
+    @Rule
+    public final ConfigSchemaRule schema = new ConfigSchemaRule()
+        .schemaPatch("io/aklivity/zilla/specs/binding/echo/schema/echo.json")
+        .configurationRoot("io/aklivity/zilla/specs/binding/echo/config");
+
+    @Test
+    public void shouldValidateServer()
+    {
+        JsonObject config = schema.validate("server.json");
+
+        assertThat(config, not(nullValue()));
+    }
+}
