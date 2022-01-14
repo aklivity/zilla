5058c46fe2dc4b02979dcfa98bfd2e014700b288
John Fallows <john.r.fallows@gmail.com>
2022-01-13 16:53:09 -0800

    Refactor cog-proxy to binding-proxy

---
+++ runtime/binding-proxy/src/main/java/io/aklivity/zilla/runtime/binding/proxy/internal/config/ProxySecureInfoConfigAdapter.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2021-2022 Aklivity Inc.
+ *
+ * Aklivity licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package io.aklivity.zilla.runtime.binding.proxy.internal.config;
+
+import jakarta.json.Json;
+import jakarta.json.JsonObject;
+import jakarta.json.JsonObjectBuilder;
+import jakarta.json.bind.adapter.JsonbAdapter;
+
+public final class ProxySecureInfoConfigAdapter implements JsonbAdapter<ProxySecureInfoConfig, JsonObject>
+{
+    private static final String VERSION_NAME = "version";
+    private static final String CIPHER_NAME = "cipher";
+    private static final String KEY_NAME = "key";
+    private static final String NAME_NAME = "name";
+    private static final String SIGNATURE_NAME = "signature";
+
+    @Override
+    public JsonObject adaptToJson(
+        ProxySecureInfoConfig secureInfo)
+    {
+        JsonObjectBuilder object = Json.createObjectBuilder();
+
+        if (secureInfo.version != null)
+        {
+            object.add(VERSION_NAME, secureInfo.version);
+        }
+
+        if (secureInfo.cipher != null)
+        {
+            object.add(CIPHER_NAME, secureInfo.cipher);
+        }
+
+        if (secureInfo.key != null)
+        {
+            object.add(KEY_NAME, secureInfo.key);
+        }
+
+        if (secureInfo.name != null)
+        {
+            object.add(NAME_NAME, secureInfo.name);
+        }
+
+        if (secureInfo.signature != null)
+        {
+            object.add(SIGNATURE_NAME, secureInfo.signature);
+        }
+
+        return object.build();
+    }
+
+    @Override
+    public ProxySecureInfoConfig adaptFromJson(
+        JsonObject object)
+    {
+        String version = object.containsKey(VERSION_NAME) ? object.getString(VERSION_NAME) : null;
+        String cipher = object.containsKey(CIPHER_NAME) ? object.getString(CIPHER_NAME) : null;
+        String key = object.containsKey(KEY_NAME) ? object.getString(KEY_NAME) : null;
+        String name = object.containsKey(NAME_NAME) ? object.getString(NAME_NAME) : null;
+        String signature = object.containsKey(SIGNATURE_NAME) ? object.getString(SIGNATURE_NAME) : null;
+
+        return new ProxySecureInfoConfig(version, cipher, key, name, signature);
+    }
+}
---
+++ runtime/binding-proxy/src/test/java/io/aklivity/zilla/runtime/binding/proxy/internal/streams/ProxyServerIT.java
@@ -0,0 +1,470 @@
+/*
+ * Copyright 2021-2022 Aklivity Inc.
+ *
+ * Aklivity licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package io.aklivity.zilla.runtime.binding.proxy.internal.streams;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.junit.rules.RuleChain.outerRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.DisableOnDebug;
+import org.junit.rules.TestRule;
+import org.junit.rules.Timeout;
+import org.kaazing.k3po.junit.annotation.Specification;
+import org.kaazing.k3po.junit.rules.K3poRule;
+
+import io.aklivity.zilla.runtime.engine.test.EngineRule;
+import io.aklivity.zilla.runtime.engine.test.annotation.Configuration;
+
+public class ProxyServerIT
+{
+    private final K3poRule k3po = new K3poRule()
+        .addScriptRoot("net", "io/aklivity/zilla/specs/binding/proxy/streams/network.v2")
+        .addScriptRoot("app", "io/aklivity/zilla/specs/binding/proxy/streams/application");
+
+    private final TestRule timeout = new DisableOnDebug(new Timeout(10, SECONDS));
+
+    private final EngineRule engine = new EngineRule()
+        .directory("target/zilla-itests")
+        .commandBufferCapacity(1024)
+        .responseBufferCapacity(1024)
+        .counterValuesBufferCapacity(4096)
+        .configurationRoot("io/aklivity/zilla/specs/binding/proxy/config")
+        .external("app0")
+        .clean();
+
+    @Rule
+    public final TestRule chain = outerRule(engine).around(k3po).around(timeout);
+
+    @Test
+    @Configuration("server.json")
+    @Specification({
+        "${net}/connected.local/client",
+        "${app}/connected.local/server" })
+    public void shouldConnectLocal() throws Exception
+    {
+        k3po.finish();
+    }
+
+    @Test
+    @Configuration("server.json")
+    @Specification({
+        "${net}/connected.local.discard/client",
+        "${app}/connected.local.discard/server" })
+    public void shouldConnectLocalDiscard() throws Exception
+    {
+        k3po.finish();
+    }
+
+    @Test
+    @Configuration("server.json")
+    @Specification({
+        "${net}/connected.local.client.sent.begin.ext/client",
+        "${app}/connected.local.client.sent.begin.ext/server" })
+    public void shouldConnectLocalClientSendsBeginExt() throws Exception
+    {
+        k3po.finish();
+    }
+
+    @Test
+    @Configuration("server.json")
+    @Specification({
+        "${net}/connected.local.client.sent.data/client",
+        "${app}/connected.local.client.sent.data/server"})
+    public void shouldConnectLocalClientSendsData() throws Exception
+    {
+        k3po.finish();
+    }
+
+    @Test
+    @Configuration("server.json")
+    @Specification({
+        "${net}/connected.local.client.sent.flush/client",
+        "${app}/connected.local.client.sent.flush/server"})
+    public void shouldConnectLocalClientSendsFlush() throws Exception
+    {
+        k3po.finish();
+    }
+
+    @Test
+    @Configuration("server.json")
+    @Specification({
+        "${net}/connected.local.client.sent.challenge/client",
+        "${app}/connected.local.client.sent.challenge/server"})
+    public void shouldConnectLocalClientSendsChallenge() throws Exception
+    {
+        k3po.finish();
+    }
+
+    @Test
+    @Configuration("server.json")
+    @Specification({
+        "${net}/connected.local.client.sent.close/client",
+        "${app}/connected.local.client.sent.close/server"})
+    public void shouldConnectLocalClientSendsClose() throws Exception
+    {
+        k3po.finish();
+    }
+
+    @Test
+    @Configuration("server.json")
+    @Specification({
+        "${net}/connected.local.client.sent.abort/client",
+        "${app}/connected.local.client.sent.abort/server"})
+    public void shouldConnectLocalClientSendsAbort() throws Exception
+    {
+        k3po.finish();
+    }
+
+    @Test
+    @Configuration("server.json")
+    @Specification({
+        "${net}/connected.local.client.sent.reset/client",
+        "${app}/connected.local.client.sent.reset/server"})
+    public void shouldConnectLocalClientSendsReset() throws Exception
+    {
+        k3po.finish();
+    }
+
+    @Test
+    @Configuration("server.json")
+    @Specification({
+        "${net}/connected.local.server.sent.data/client",
+        "${app}/connected.local.server.sent.data/server"})
+    public void shouldConnectLocalServerSendsData() throws Exception
+    {
+        k3po.finish();
+    }
+
+    @Test
+    @Configuration("server.json")
+    @Specification({
+        "${net}/connected.local.server.sent.flush/client",
+        "${app}/connected.local.server.sent.flush/server"})
+    public void shouldConnectLocalServerSendsFlush() throws Exception
+    {
+        k3po.finish();
+    }
+
+    @Test
+    @Configuration("server.json")
+    @Specification({
+        "${net}/connected.local.server.sent.challenge/client",
+        "${app}/connected.local.server.sent.challenge/server"})
+    public void shouldConnectLocalServerSendsChallenge() throws Exception
+    {
+        k3po.finish();
+    }
+
+    @Test
+    @Configuration("server.json")
+    @Specification({
+        "${net}/connected.local.server.sent.close/client",
+        "${app}/connected.local.server.sent.close/server"})
+    public void shouldConnectLocalServerSendsClose() throws Exception
+    {
+        k3po.finish();
+    }
+
+    @Test
+    @Configuration("server.json")
+    @Specification({
+        "${net}/connected.local.server.sent.abort/client",
+        "${app}/connected.local.server.sent.abort/server"})
+    public void shouldConnectLocalServerSendsAbort() throws Exception
+    {
+        k3po.finish();
+    }
+
+    @Test
+    @Configuration("server.json")
+    @Specification({
+        "${net}/connected.local.server.sent.reset/client",
+        "${app}/connected.local.server.sent.reset/server"})
+    public void shouldConnectLocalServerSendsReset() throws Exception
+    {
+        k3po.finish();
+    }
+
+    @Test
+    @Configuration("server.tcp4.json")
+    @Specification({
+        "${net}/connected.tcp4/client",
+        "${app}/connected.tcp4/server"})
+    public void shouldConnectTcp4() throws Exception
+    {
+        k3po.finish();
+    }
+
+    @Test
+    @Configuration("server.tcp4.alpn.json")
+    @Specification({
+        "${net}/connected.tcp4.alpn/client",
+        "${app}/connected.tcp4.alpn/server"})
+    public void shouldConnectTcp4WithAlpn() throws Exception
+    {
+        k3po.finish();
+    }
+
+    @Test
+    @Configuration("server.json")
+    @Specification({
+        "${net}/connected.tcp4.authority/client",
+        "${app}/connected.tcp4.authority/server"})
+    public void shouldConnectTcp4WithAuthority() throws Exception
+    {
+        k3po.finish();
+    }
+
+    @Test
+    @Configuration("server.json")
+    @Specification({
+        "${net}/connected.tcp4.crc32c/client",
+        "${app}/connected.tcp4.crc32c/server"})
+    public void shouldConnectTcp4WithCrc32c() throws Exception
+    {
+        k3po.finish();
+    }
+
+    @Test
+    @Configuration("server.json")
+    @Specification({
+        "${net}/connected.tcp4.identity/client",
+        "${app}/connected.tcp4.identity/server"})
+    public void shouldConnectTcp4WithIdentity() throws Exception
+    {
+        k3po.finish();
+    }
+
+    @Test
+    @Configuration("server.json")
+    @Specification({
+        "${net}/connected.tcp4.namespace/client",
+        "${app}/connected.tcp4.namespace/server"})
+    public void shouldConnectTcp4WithNamespace() throws Exception
+    {
+        k3po.finish();
+    }
+
+    @Test
+    @Configuration("server.json")
+    @Specification({
+        "${net}/connected.tcp4.noop/client",
+        "${app}/connected.tcp4.noop/server"})
+    public void shouldConnectTcp4WithNoop() throws Exception
+    {
+        k3po.finish();
+    }
+
+    @Test
+    @Configuration("server.json")
+    @Specification({
+        "${net}/connected.tcp4.ssl/client",
+        "${app}/connected.tcp4.ssl/server"})
+    public void shouldConnectTcp4WithSsl() throws Exception
+    {
+        k3po.finish();
+    }
+
+    @Test
+    @Configuration("server.json")
+    @Specification({
+        "${net}/connected.tcp4.ssl.client.cert/client",
+        "${app}/connected.tcp4.ssl.client.cert/server"})
+    public void shouldConnectTcp4WithSslClientCertificate() throws Exception
+    {
+        k3po.finish();
+    }
+
+    @Test
+    @Configuration("server.json")
+    @Specification({
+        "${net}/connected.tcp4.ssl.client.cert.session/client",
+        "${app}/connected.tcp4.ssl.client.cert.session/server"})
+    public void shouldConnectTcp4WithSslClientCertificateSession() throws Exception
+    {
+        k3po.finish();
+    }
+
+    @Test
+    @Configuration("server.json")
+    @Specification({
+        "${net}/connected.tcp4.ssl.experimental/client",
+        "${app}/connected.tcp4.ssl.experimental/server"})
+    public void shouldConnectTcp4WithSslExperimental() throws Exception
+    {
+        k3po.finish();
+    }
+
+    @Test
+    @Configuration("server.json")
+    @Specification({
+        "${net}/connected.tcp4.experimental/client",
+        "${app}/connected.tcp4.experimental/server"})
+    public void shouldConnectTcp4WithExperimental() throws Exception
+    {
+        k3po.finish();
+    }
+
+    @Test
+    @Configuration("server.json")
+    @Specification({
+        "${net}/connected.udp4/client",
+        "${app}/connected.udp4/server"})
+    public void shouldConnectUdp4() throws Exception
+    {
+        k3po.finish();
+    }
+
+    @Test
+    @Configuration("server.tcp6.json")
+    @Specification({
+        "${net}/connected.tcp6/client",
+        "${app}/connected.tcp6/server"})
+    public void shouldConnectTcp6() throws Exception
+    {
+        k3po.finish();
+    }
+
+    @Test
+    @Configuration("server.json")
+    @Specification({
+        "${net}/connected.udp6/client",
+        "${app}/connected.udp6/server"})
+    public void shouldConnectUdp6() throws Exception
+    {
+        k3po.finish();
+    }
+
+    @Test
+    @Configuration("server.sock.stream.json")
+    @Specification({
+        "${net}/connected.sock.stream/client",
+        "${app}/connected.sock.stream/server"})
+    public void shouldConnectSockStream() throws Exception
+    {
+        k3po.finish();
+    }
+
+    @Test
+    @Configuration("server.json")
+    @Specification({
+        "${net}/connected.sock.datagram/client",
+        "${app}/connected.sock.datagram/server"})
+    public void shouldConnectSockDatagram() throws Exception
+    {
+        k3po.finish();
+    }
+
+    @Test
+    @Configuration("server.json")
+    @Specification({
+        "${net}/rejected.header.mismatch/client"})
+    public void shouldRejectHeaderMismatch() throws Exception
+    {
+        k3po.finish();
+    }
+
+    @Test
+    @Configuration("server.json")
+    @Specification({
+        "${net}/rejected.header.version.mismatch/client"})
+    public void shouldRejectHeaderVersionMismatch() throws Exception
+    {
+        k3po.finish();
+    }
+
+    @Test
+    @Configuration("server.json")
+    @Specification({
+        "${net}/rejected.tcp4.crc32c.mismatch/client"})
+    public void shouldRejectTcp4WithCrc32cMismatch() throws Exception
+    {
+        k3po.finish();
+    }
+
+    @Test
+    @Configuration("server.json")
+    @Specification({
+        "${net}/rejected.tcp4.crc32c.overflow/client"})
+    public void shouldRejectTcp4WithCrc32cOverflow() throws Exception
+    {
+        k3po.finish();
+    }
+
+    @Test
+    @Configuration("server.json")
+    @Specification({
+        "${net}/rejected.tcp4.crc32c.underflow/client"})
+    public void shouldRejectTcp4WithCrc32cUnderflow() throws Exception
+    {
+        k3po.finish();
+    }
+
+    @Test
+    @Configuration("server.json")
+    @Specification({
+        "${net}/rejected.tcp4.ssl.underflow/client"})
+    public void shouldRejectTcp4WithSslUnderflow() throws Exception
+    {
+        k3po.finish();
+    }
+
+    @Test
+    @Configuration("server.json")
+    @Specification({
+        "${net}/rejected.tcp4.underflow/client"})
+    public void shouldRejectTcp4Underflow() throws Exception
+    {
+        k3po.finish();
+    }
+
+    @Test
+    @Configuration("server.json")
+    @Specification({
+        "${net}/rejected.tcp6.underflow/client"})
+    public void shouldRejectTcp6Underflow() throws Exception
+    {
+        k3po.finish();
+    }
+
+    @Test
+    @Configuration("server.json")
+    @Specification({
+        "${net}/rejected.sock.stream.underflow/client"})
+    public void shouldRejectSockStreamUnderflow() throws Exception
+    {
+        k3po.finish();
+    }
+
+    @Test
+    @Configuration("server.json")
+    @Specification({
+        "${net}/rejected.address.family.mismatch/client"})
+    public void shouldRejectAddressFamilyMismatch() throws Exception
+    {
+        k3po.finish();
+    }
+
+    @Test
+    @Configuration("server.json")
+    @Specification({
+        "${net}/rejected.command.mismatch/client"})
+    public void shouldRejectCommandMismatch() throws Exception
+    {
+        k3po.finish();
+    }
+}
