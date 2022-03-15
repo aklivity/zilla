bccdd1e2f8767c45bd0cac112d9dffc889c54229
John Fallows <john.r.fallows@gmail.com>
2022-03-15 15:22:39 -0700

    Test verify JWT roles when index differs

--- incubator/guard-jwt/src/test/java/io/aklivity/zilla/runtime/guard/jwt/internal/JwtGuardTest.java
+++ incubator/guard-jwt/src/test/java/io/aklivity/zilla/runtime/guard/jwt/internal/JwtGuardTest.java
@@ -214,4 +214,83 @@ public class JwtGuardTest
 
         assertTrue(verifier.test(sessionId));
     }
+
+    @Test
+    public void shouldVerifyRolesWhenEmpty() throws Exception
+    {
+        EngineContext engine = Mockito.mock(EngineContext.class);
+
+        when(engine.index()).thenReturn(0);
+        when(engine.supplyAuthorizedId()).thenReturn(1L);
+
+        GuardedConfig guarded = new GuardedConfig("test0", asList());
+
+        Configuration config = new Configuration();
+        GuardFactory factory = GuardFactory.instantiate();
+        Guard guard = factory.create("jwt", config);
+
+        GuardContext context = guard.supply(engine);
+
+        Duration challenge = ofSeconds(3L);
+        JwtOptionsConfig options =
+                new JwtOptionsConfig("test issuer", "testAudience", singletonList(RFC7515_RS256_CONFIG), challenge);
+        GuardHandler handler = context.attach(new GuardConfig("test0", "jwt", options));
+
+        LongPredicate verifier = guard.verifier(s -> 0, guarded);
+
+        Instant now = Instant.now();
+
+        JwtClaims claims = new JwtClaims();
+        claims.setClaim("iss", "test issuer");
+        claims.setClaim("aud", "testAudience");
+        claims.setClaim("sub", "testSubject");
+        claims.setClaim("exp", now.getEpochSecond() + 10L);
+        claims.setClaim("scope", "read:stream write:stream");
+
+        String token = sign(claims.toJson(), "test", RFC7515_RS256, "RS256");
+
+        long sessionId = handler.reauthorize(101L, token);
+
+        assertTrue(verifier.test(sessionId));
+    }
+
+    @Test
+    public void shouldVerifyWhenIndexDiffers() throws Exception
+    {
+        EngineContext engine = Mockito.mock(EngineContext.class);
+
+        when(engine.index()).thenReturn(0);
+        when(engine.supplyAuthorizedId()).thenReturn(0x01L);
+
+        GuardFactory factory = GuardFactory.instantiate();
+        Guard guard = factory.create("jwt", new Configuration());
+
+        GuardContext context = guard.supply(engine);
+
+        Duration challenge = ofSeconds(3L);
+        JwtOptionsConfig options =
+                new JwtOptionsConfig("test issuer", "testAudience", singletonList(RFC7515_RS256_CONFIG), challenge);
+        GuardConfig config = new GuardConfig("test0", "jwt", options);
+        config.id = 0x11L;
+        GuardHandler handler = context.attach(config);
+
+        GuardedConfig guarded = new GuardedConfig("test0", asList());
+        guarded.id = config.id;
+        LongPredicate verifier = guard.verifier(id -> (int)(id >> 4), guarded);
+
+        Instant now = Instant.now();
+
+        JwtClaims claims = new JwtClaims();
+        claims.setClaim("iss", "test issuer");
+        claims.setClaim("aud", "testAudience");
+        claims.setClaim("sub", "testSubject");
+        claims.setClaim("exp", now.getEpochSecond() + 10L);
+        claims.setClaim("scope", "read:stream write:stream");
+
+        String token = sign(claims.toJson(), "test", RFC7515_RS256, "RS256");
+
+        long sessionId = handler.reauthorize(101L, token);
+
+        assertTrue(verifier.test(sessionId));
+    }
 }
