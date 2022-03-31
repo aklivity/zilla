63e7bab14593913b850ad82663dc149ae27594a4
John Fallows <john.r.fallows@gmail.com>
2022-03-30 17:03:37 -0700

    Scenarios for http-kafka binding

---
+++ incubator/binding-http-kafka.spec/src/test/java/io/aklivity/zilla/specs/binding/http/kafka/streams/HttpIT.java
@@ -0,0 +1,136 @@
+/*
+ * Copyright 2021-2022 Aklivity Inc
+ *
+ * Licensed under the Aklivity Community License (the "License"); you may not use
+ * this file except in compliance with the License.  You may obtain a copy of the
+ * License at
+ *
+ *   https://www.aklivity.io/aklivity-community-license/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package io.aklivity.zilla.specs.binding.http.kafka.streams;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.junit.rules.RuleChain.outerRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.DisableOnDebug;
+import org.junit.rules.TestRule;
+import org.junit.rules.Timeout;
+import org.kaazing.k3po.junit.annotation.Specification;
+import org.kaazing.k3po.junit.rules.K3poRule;
+
+public class HttpIT
+{
+    private final K3poRule k3po = new K3poRule()
+        .addScriptRoot("http", "io/aklivity/zilla/specs/binding/http/kafka/streams/http");
+
+    private final TestRule timeout = new DisableOnDebug(new Timeout(5, SECONDS));
+
+    @Rule
+    public final TestRule chain = outerRule(k3po).around(timeout);
+
+    @Test
+    @Specification({
+        "${http}/delete.item/client",
+        "${http}/delete.item/server"})
+    public void shouldDeleteItem() throws Exception
+    {
+        k3po.finish();
+    }
+
+    @Test
+    @Specification({
+        "${http}/delete.item.async/client",
+        "${http}/delete.item.async/server"})
+    public void shouldDeleteItemAsync() throws Exception
+    {
+        k3po.finish();
+    }
+
+    @Test
+    @Specification({
+        "${http}/delete.item.sync/client",
+        "${http}/delete.item.sync/server"})
+    public void shouldDeleteItemSync() throws Exception
+    {
+        k3po.finish();
+    }
+
+    @Test
+    @Specification({
+        "${http}/get.item/client",
+        "${http}/get.item/server"})
+    public void shouldGetItem() throws Exception
+    {
+        k3po.finish();
+    }
+
+    @Test
+    @Specification({
+        "${http}/get.items/client",
+        "${http}/get.items/server"})
+    public void shouldGetItems() throws Exception
+    {
+        k3po.finish();
+    }
+
+    @Test
+    @Specification({
+        "${http}/patch.item/client",
+        "${http}/patch.item/server"})
+    public void shouldPatchItem() throws Exception
+    {
+        k3po.finish();
+    }
+
+    @Test
+    @Specification({
+        "${http}/patch.item.async/client",
+        "${http}/patch.item.async/server"})
+    public void shouldPatchItemAsync() throws Exception
+    {
+        k3po.finish();
+    }
+
+    @Test
+    @Specification({
+        "${http}/patch.item.sync/client",
+        "${http}/patch.item.sync/server"})
+    public void shouldPatchItemSync() throws Exception
+    {
+        k3po.finish();
+    }
+
+    @Test
+    @Specification({
+        "${http}/put.item/client",
+        "${http}/put.item/server"})
+    public void shouldPutItem() throws Exception
+    {
+        k3po.finish();
+    }
+
+    @Test
+    @Specification({
+        "${http}/put.item.async/client",
+        "${http}/put.item.async/server"})
+    public void shouldPutItemAsync() throws Exception
+    {
+        k3po.finish();
+    }
+
+    @Test
+    @Specification({
+        "${http}/put.item.sync/client",
+        "${http}/put.item.sync/server"})
+    public void shouldPutItemSync() throws Exception
+    {
+        k3po.finish();
+    }
+}
