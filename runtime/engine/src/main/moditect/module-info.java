fea4c695870ce53107131754a3543b79f4b6ba2a
Ankit Kumar <ankitjames25@gmail.com>
2023-01-25 07:23:30 +0530

    Adding support for Expression Resolver (#143)
    * Adding support for Expression Resolver
    * Updating Expression Config
    * Adding unit test for ExpressionResolver
    * Updating log to avoid printing sensitive/derived info

--- runtime/engine/src/main/moditect/module-info.java
+++ runtime/engine/src/main/moditect/module-info.java
@@ -46,4 +46,8 @@ module io.aklivity.zilla.runtime.engine
     uses io.aklivity.zilla.runtime.engine.guard.GuardFactorySpi;
     uses io.aklivity.zilla.runtime.engine.vault.VaultFactorySpi;
     uses io.aklivity.zilla.runtime.engine.ext.EngineExtSpi;
+    uses io.aklivity.zilla.runtime.engine.expression.ExpressionResolverSpi;
+
+    provides io.aklivity.zilla.runtime.engine.expression.ExpressionResolverSpi
+            with io.aklivity.zilla.runtime.engine.internal.expression.EnvironmentResolverSpi;
 }
