202aa74df39a4bc321035dba7322addbc4378851
Attila Kreiner <kreiner.attila@gmail.com>
2023-03-16 17:14:33 +0100

    WIP metrics command

---
+++ incubator/command-metrics/src/main/java/io/aklivity/zilla/runtime/command/metrics/internal/airline/ZillaMetricsCommand.java
@@ -0,0 +1,168 @@
+/*
+ * Copyright 2021-2022 Aklivity Inc.
+ *
+ * Aklivity licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package io.aklivity.zilla.runtime.command.metrics.internal.airline;
+
+import static java.util.stream.Collectors.toList;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
+import org.agrona.LangUtil;
+import org.agrona.collections.Int2ObjectHashMap;
+
+import com.github.rvesse.airline.annotations.Arguments;
+import com.github.rvesse.airline.annotations.Command;
+import com.github.rvesse.airline.annotations.Option;
+
+import io.aklivity.zilla.runtime.command.ZillaCommand;
+import io.aklivity.zilla.runtime.command.metrics.internal.labels.LabelManager;
+import io.aklivity.zilla.runtime.command.metrics.internal.layout.CountersLayout;
+import io.aklivity.zilla.runtime.command.metrics.internal.layout.CountersReader;
+
+@Command(name = "metrics", description = "Show engine metrics")
+public final class ZillaMetricsCommand extends ZillaCommand
+{
+    private static final Path METRICS_DIRECTORY = Paths.get(".zilla", "engine", "metrics");
+    private static final Path LABELS_DIRECTORY = Paths.get(".zilla", "engine");
+    private static final Pattern COUNTERS_PATTERN = Pattern.compile("counters(\\d+)");
+
+    @Option(name = { "--namespace" })
+    public String namespace;
+
+    @Arguments(title = {"name"})
+    public List<String> args;
+
+    private final Map<Path, CountersReader> readersByPath;
+    private final LabelManager labels;
+    private final Map<Integer, Map<Integer, Map<Integer, Long>>> metrics; // namespace -> binding -> metric -> value
+
+    public ZillaMetricsCommand()
+    {
+        this.readersByPath = new LinkedHashMap<>();
+        this.labels = new LabelManager(LABELS_DIRECTORY);
+        this.metrics = new Int2ObjectHashMap<>();
+    }
+
+    @Override
+    public void run()
+    {
+        //String name = args != null && args.size() >= 1 ? args.get(0) : null;
+        //LongPredicate filter = filterBy(namespace, name); // TODO: Ati - filtering
+
+        try (Stream<Path> files = Files.walk(METRICS_DIRECTORY, 1))
+        {
+            System.out.println("namespace\tbinding\tmetric\tvalue"); // TODO: Ati - formatting
+            for (CountersReader reader : readers(files))
+            {
+                for (long[] record : reader.records())
+                {
+                    // TODO: Ati - filtering
+                    addRecordToMetrics(record);
+                }
+            }
+            printMetrics();
+        }
+        catch (IOException ex)
+        {
+            LangUtil.rethrowUnchecked(ex);
+        }
+    }
+
+    private List<CountersReader> readers(
+        Stream<Path> files)
+    {
+        return files.filter(this::isCountersFile)
+                .map(this::supplyCounterReaders)
+                .collect(toList());
+    }
+
+    private boolean isCountersFile(
+        Path path)
+    {
+        return path.getNameCount() - METRICS_DIRECTORY.getNameCount() == 1 &&
+                COUNTERS_PATTERN.matcher(path.getName(path.getNameCount() - 1).toString()).matches() &&
+                Files.isRegularFile(path);
+    }
+
+    private CountersReader supplyCounterReaders(
+        Path path)
+    {
+        return readersByPath.computeIfAbsent(path, this::newCountersReader);
+    }
+
+    private CountersReader newCountersReader(
+        Path loadPath)
+    {
+        CountersLayout layout = new CountersLayout.Builder().path(loadPath).build();
+        return new CountersReader(layout);
+    }
+
+    private void addRecordToMetrics(
+        long[] record)
+    {
+        int namespaceId = namespaceId(record[0]);
+        int bindingId = localId(record[0]);
+        int metricId = localId(record[1]);
+        long value = record[2];
+
+        metrics.putIfAbsent(namespaceId, new Int2ObjectHashMap<>());
+        Map<Integer, Map<Integer, Long>> metricsByNamespace = metrics.get(namespaceId);
+
+        metricsByNamespace.putIfAbsent(bindingId, new Int2ObjectHashMap<>());
+        Map<Integer, Long> metricsByBinding = metricsByNamespace.get(bindingId);
+
+        Long previousCount = metricsByBinding.getOrDefault(metricId, 0L);
+        Long currentCount = previousCount + value; // this works only for counters
+        metricsByBinding.put(metricId, currentCount);
+    }
+
+    private static int namespaceId(
+        long packedId)
+    {
+        return (int) (packedId >> Integer.SIZE) & 0xffff_ffff;
+    }
+
+    private static int localId(
+        long packedId)
+    {
+        return (int) (packedId >> 0) & 0xffff_ffff;
+    }
+
+    private void printMetrics()
+    {
+        for (Integer namespaceId : metrics.keySet())
+        {
+            for (Integer bindingId : metrics.get(namespaceId).keySet())
+            {
+                for (Integer metricId : metrics.get(namespaceId).get(bindingId).keySet())
+                {
+                    String namespace = labels.lookupLabel(namespaceId);
+                    String binding = labels.lookupLabel(bindingId);
+                    String metric = labels.lookupLabel(metricId);
+                    Long value = metrics.get(namespaceId).get(bindingId).get(metricId);
+                    System.out.format("%s\t%s\t%s\t%d\n", namespace, binding, metric, value);
+                }
+            }
+        }
+    }
+}
---
+++ incubator/command-metrics/src/main/java/io/aklivity/zilla/runtime/command/metrics/internal/layout/CountersLayout.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2021-2022 Aklivity Inc.
+ *
+ * Aklivity licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package io.aklivity.zilla.runtime.command.metrics.internal.layout;
+
+import static org.agrona.IoUtil.mapExistingFile;
+
+import java.io.File;
+import java.nio.MappedByteBuffer;
+import java.nio.file.Path;
+import java.util.Iterator;
+
+import org.agrona.BitUtil;
+import org.agrona.concurrent.AtomicBuffer;
+import org.agrona.concurrent.UnsafeBuffer;
+
+public final class CountersLayout implements Iterable<long[]>
+{
+    // We use the buffer to store structs {long bindingId, long metricId, long value}
+    private static final int FIELD_SIZE = BitUtil.SIZE_OF_LONG;
+    private static final int RECORD_SIZE = 3 * FIELD_SIZE;
+    private static final int BINDING_ID_OFFSET = 0;
+    private static final int METRIC_ID_OFFSET = 1 * FIELD_SIZE;
+    private static final int VALUE_OFFSET = 2 * FIELD_SIZE;
+
+    private final AtomicBuffer buffer;
+
+    private CountersLayout(
+        AtomicBuffer buffer)
+    {
+        this.buffer = buffer;
+    }
+
+    public CountersIterator iterator()
+    {
+        return new CountersIterator();
+    }
+
+    public final class CountersIterator implements Iterator<long[]>
+    {
+        private int index = 0;
+
+        @Override
+        public boolean hasNext()
+        {
+            return isBufferLeft() && isRecordLeft();
+        }
+
+        private boolean isBufferLeft()
+        {
+            return index < buffer.capacity();
+        }
+
+        private boolean isRecordLeft()
+        {
+            return buffer.getLong(index + BINDING_ID_OFFSET) != 0L;
+        }
+
+        @Override
+        public long[] next()
+        {
+            long bindingId = buffer.getLong(index + BINDING_ID_OFFSET);
+            long metricId = buffer.getLong(index + METRIC_ID_OFFSET);
+            long value = buffer.getLong(index + VALUE_OFFSET);
+            index += RECORD_SIZE;
+            return new long[]{bindingId, metricId, value};
+        }
+    }
+
+    public static final class Builder
+    {
+        private Path path;
+
+        public Builder path(
+            Path path)
+        {
+            this.path = path;
+            return this;
+        }
+
+        public CountersLayout build()
+        {
+            final File layoutFile = path.toFile();
+            final MappedByteBuffer mappedBuffer = mapExistingFile(layoutFile, "counters");
+            final AtomicBuffer atomicBuffer = new UnsafeBuffer(mappedBuffer);
+            return new CountersLayout(atomicBuffer);
+        }
+    }
+}
