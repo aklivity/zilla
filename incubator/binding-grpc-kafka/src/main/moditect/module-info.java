dc32c5ece71d21c9f4e7c62490957b1247dd0494
Akram  Yakubov <akram.yakubov@gmail.com>
2023-03-21 22:04:50 -0700

    Checkpoint

--- incubator/binding-grpc-kafka/src/main/moditect/module-info.java
+++ incubator/binding-grpc-kafka/src/main/moditect/module-info.java
@@ -21,4 +21,7 @@ module io.aklivity.zilla.runtime.binding.grpc.kafka
 
     provides io.aklivity.zilla.runtime.engine.config.ConditionConfigAdapterSpi
         with io.aklivity.zilla.runtime.binding.grpc.kafka.internal.config.GrpcKafkaConditionConfigAdapter;
+
+    provides io.aklivity.zilla.runtime.engine.config.WithConfigAdapterSpi
+        with io.aklivity.zilla.runtime.binding.grpc.kafka.internal.config.GrpcKafkaWithConfigAdapter;
 }
