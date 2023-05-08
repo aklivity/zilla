6421f2a911638b4ac8012824255ad0c45a5ecab5
bmaidics <b.maidics@gmail.com>
2023-05-08 09:59:59 +0200

    Change to correct filters functions in scenarios

--- incubator/binding-mqtt-kafka.spec/src/test/java/io/aklivity/zilla/specs/binding/mqtt/kafka/config/SchemaTest.java
+++ incubator/binding-mqtt-kafka.spec/src/test/java/io/aklivity/zilla/specs/binding/mqtt/kafka/config/SchemaTest.java
@@ -1,17 +1,16 @@
 /*
- * Copyright 2021-2022 Aklivity Inc.
+ * Copyright 2021-2022 Aklivity Inc
  *
- * Aklivity licenses this file to you under the Apache License,
- * version 2.0 (the "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at:
+ * Licensed under the Aklivity Community License (the "License"); you may not use
+ * this file except in compliance with the License.  You may obtain a copy of the
+ * License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ *   https://www.aklivity.io/aklivity-community-license/
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations
- * under the License.
+ * WARRANTIES OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 package io.aklivity.zilla.specs.binding.mqtt.kafka.config;
 
@@ -21,7 +20,6 @@ import static org.hamcrest.Matchers.nullValue;
 
 import jakarta.json.JsonObject;
 
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -39,14 +37,6 @@ public class SchemaTest
     public void shouldValidateProxy()
     {
         JsonObject config = schema.validate("proxy.yaml");
-
-        assertThat(config, not(nullValue()));
-    }
-
-    @Test
-    public void shouldValidateProxyWhenTopic()
-    {
-        JsonObject config = schema.validate("proxy.when.topic.yaml");
 
         assertThat(config, not(nullValue()));
     }
