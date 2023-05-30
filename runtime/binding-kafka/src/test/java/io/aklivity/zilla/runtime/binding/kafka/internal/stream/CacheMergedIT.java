2c2b1e9ab9a694b4b9c360e5860e7e949c0a39c1
Ankit Kumar <ankitjames25@gmail.com>
2023-05-30 23:19:45 +0530

    CacheMergedIT.shouldFetchMergedMessagesWithIsolationReadCommitted fix (#260)

--- runtime/binding-kafka/src/test/java/io/aklivity/zilla/runtime/binding/kafka/internal/stream/CacheMergedIT.java
+++ runtime/binding-kafka/src/test/java/io/aklivity/zilla/runtime/binding/kafka/internal/stream/CacheMergedIT.java
@@ -192,6 +192,12 @@ public class CacheMergedIT
         k3po.awaitBarrier("RECEIVED_MESSAGE_B2");
         k3po.notifyBarrier("SEND_MESSAGE_A3");
         k3po.notifyBarrier("SEND_MESSAGE_B3");
+        k3po.awaitBarrier("RECEIVED_MESSAGE_B3");
+        k3po.notifyBarrier("SEND_MESSAGE_A4");
+        k3po.awaitBarrier("RECEIVED_MESSAGE_B4");
+        k3po.notifyBarrier("SEND_MESSAGE_A5");
+        k3po.awaitBarrier("RECEIVED_MESSAGE_B5");
+        k3po.notifyBarrier("SEND_MESSAGE_A6");
         k3po.finish();
     }
 
@@ -277,7 +283,6 @@ public class CacheMergedIT
         k3po.finish();
     }
 
-    @Ignore("GitHub Actions")
     @Test
     @Configuration("cache.options.merged.yaml")
     @Specification({
@@ -289,6 +294,12 @@ public class CacheMergedIT
         k3po.awaitBarrier("RECEIVED_MESSAGE_B2");
         k3po.notifyBarrier("SEND_MESSAGE_A3");
         k3po.notifyBarrier("SEND_MESSAGE_B3");
+        k3po.awaitBarrier("RECEIVED_MESSAGE_B3");
+        k3po.notifyBarrier("SEND_MESSAGE_A4");
+        k3po.awaitBarrier("RECEIVED_MESSAGE_B4");
+        k3po.notifyBarrier("SEND_MESSAGE_A5");
+        k3po.awaitBarrier("RECEIVED_MESSAGE_B5");
+        k3po.notifyBarrier("SEND_MESSAGE_A6");
         k3po.finish();
     }
 
