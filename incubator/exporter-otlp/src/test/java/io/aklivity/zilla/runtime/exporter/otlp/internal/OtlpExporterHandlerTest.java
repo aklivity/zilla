3d7c047595ed1e5be1d5955f051ba8c0da2a9ce7
Attila Kreiner <kreiner.attila@gmail.com>
2023-06-16 15:22:46 +0200

    add tests

---
+++ incubator/exporter-otlp/src/test/java/io/aklivity/zilla/runtime/exporter/otlp/internal/OtlpExporterHandlerTest.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2021-2023 Aklivity Inc
+ *
+ * Licensed under the Aklivity Community License (the "License"); you may not use
+ * this file except in compliance with the License.  You may obtain a copy of the
+ * License at
+ *
+ *   https://www.aklivity.io/aklivity-community-license/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package io.aklivity.zilla.runtime.exporter.otlp.internal;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.mock;
+
+import java.util.List;
+import java.util.function.Function;
+
+import org.junit.Test;
+
+import io.aklivity.zilla.runtime.engine.EngineConfiguration;
+import io.aklivity.zilla.runtime.engine.EngineContext;
+import io.aklivity.zilla.runtime.engine.config.AttributeConfig;
+import io.aklivity.zilla.runtime.engine.config.ExporterConfig;
+import io.aklivity.zilla.runtime.engine.config.KindConfig;
+import io.aklivity.zilla.runtime.engine.config.OptionsConfig;
+import io.aklivity.zilla.runtime.exporter.otlp.internal.config.OtlpEndpointConfig;
+import io.aklivity.zilla.runtime.exporter.otlp.internal.config.OtlpExporterConfig;
+import io.aklivity.zilla.runtime.exporter.otlp.internal.config.OtlpOptionsConfig;
+
+public class OtlpExporterHandlerTest
+{
+    @Test
+    public void shouldInstantiate()
+    {
+        // GIVEN
+        EngineConfiguration config = mock(EngineConfiguration.class);
+        EngineContext context = mock(EngineContext.class);
+        OtlpEndpointConfig endpoint = new OtlpEndpointConfig("http://example.com");
+        OptionsConfig options = new OtlpOptionsConfig(30L, new OtlpEndpointConfig[]{endpoint});
+        ExporterConfig exporterConfig = new ExporterConfig("otlp0", "otlp", options);
+        OtlpExporterConfig exporter = new OtlpExporterConfig(exporterConfig);
+        Function<String, KindConfig> findBindingKind = mock(Function.class);
+        List<AttributeConfig> attributes = List.of();
+
+        // WHEN
+        OltpExporterHandler handler = new OltpExporterHandler(config, context, exporter, findBindingKind, attributes);
+
+        // THEN
+        assertThat(handler, instanceOf(OltpExporterHandler.class));
+    }
+}
