e68e1eecc660be269754ad972950171ab7af8682
bmaidics <b.maidics@gmail.com>
2023-06-27 18:33:11 +0200

    Address review comments

--- runtime/binding-kafka/src/main/java/io/aklivity/zilla/runtime/binding/kafka/internal/stream/KafkaMergedFactory.java
+++ runtime/binding-kafka/src/main/java/io/aklivity/zilla/runtime/binding/kafka/internal/stream/KafkaMergedFactory.java
@@ -1635,10 +1635,7 @@ public final class KafkaMergedFactory implements BindingHandler
             final long latestOffset = partition.latestOffset();
             final long stableOffset = partition.stableOffset();
 
-            if (nextOffsetsById.getOrDefault((long) partitionId, 0L) < partitionOffset)
-            {
-                nextOffsetsById.put(partitionId, partitionOffset);
-            }
+            nextOffsetsById.put(partitionId, Math.max(partitionOffset, nextOffsetsById.get(partitionId)));
             initialLatestOffsetsById.put(partitionId, latestOffset);
             initialStableOffsetsById.put(partitionId, stableOffset);
 
