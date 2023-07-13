48e06ddf599e6a8dd6552c40cfe2adee5b1b5116
Attila Kreiner <kreiner.attila@gmail.com>
2023-07-13 12:38:08 +0200

    WIP export()

--- incubator/exporter-otlp/src/main/java/io/aklivity/zilla/runtime/exporter/otlp/internal/OltpExporterHandler.java
+++ incubator/exporter-otlp/src/main/java/io/aklivity/zilla/runtime/exporter/otlp/internal/OltpExporterHandler.java
@@ -14,10 +14,14 @@
  */
 package io.aklivity.zilla.runtime.exporter.otlp.internal;
 
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.time.Duration;
 import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.concurrent.CompletableFuture;
 import java.util.function.IntFunction;
 
 import io.aklivity.zilla.runtime.engine.EngineConfiguration;
@@ -26,7 +30,6 @@ import io.aklivity.zilla.runtime.engine.config.AttributeConfig;
 import io.aklivity.zilla.runtime.engine.config.KindConfig;
 import io.aklivity.zilla.runtime.engine.exporter.ExporterHandler;
 import io.aklivity.zilla.runtime.engine.metrics.Collector;
-import io.aklivity.zilla.runtime.engine.metrics.reader.MetricRecord;
 import io.aklivity.zilla.runtime.engine.metrics.reader.MetricsReader;
 import io.aklivity.zilla.runtime.exporter.otlp.internal.config.OtlpEndpointConfig;
 import io.aklivity.zilla.runtime.exporter.otlp.internal.config.OtlpExporterConfig;
@@ -34,15 +37,21 @@ import io.aklivity.zilla.runtime.exporter.otlp.internal.serializer.OtlpMetricsSe
 
 public class OltpExporterHandler implements ExporterHandler
 {
-    private static final long DELAY = Duration.ofSeconds(1).toMillis();
+    private static final long RETRY_INTERVAL = Duration.ofSeconds(5).toMillis();
+    private static final long WARNING_INTERVAL = Duration.ofMinutes(5).toMillis();
 
     private final EngineContext context;
-    private final OtlpEndpointConfig endpoint;
-    private final Duration interval;
+    private final URI url;
+    private final long interval;
     private final Collector collector;
     private final IntFunction<KindConfig> resolveKind;
     private final List<AttributeConfig> attributes;
-    private final Timer timer;
+    private final HttpClient httpClient;
+
+    private OtlpMetricsSerializer serializer;
+    private long lastAttempt;
+    private long lastSuccess;
+    private boolean warning;
 
     public OltpExporterHandler(
         EngineConfiguration config,
@@ -53,33 +62,73 @@ public class OltpExporterHandler implements ExporterHandler
         List<AttributeConfig> attributes)
     {
         this.context = context;
-        this.endpoint = exporter.options().endpoints[0];
-        this.interval = Duration.ofSeconds(exporter.options().interval);
-        this.attributes = attributes;
+        OtlpEndpointConfig endpoint = exporter.options().endpoints[0];
+        this.url = URI.create(endpoint.url);
+        this.interval = Duration.ofSeconds(exporter.options().interval).toMillis();
         this.collector = collector;
         this.resolveKind = resolveKind;
-        this.timer = new Timer();
+        this.attributes = attributes;
+        this.httpClient = HttpClient.newBuilder().build();
     }
 
     @Override
     public void start()
     {
         MetricsReader metrics = new MetricsReader(collector, context::supplyLocalName);
-        List<MetricRecord> records = metrics.records();
-        OtlpMetricsSerializer serializer = new OtlpMetricsSerializer(records, attributes, context::resolveMetric, resolveKind);
-        TimerTask task = new OtlpExporterTask(endpoint.url, serializer);
-        timer.schedule(task, DELAY, interval.toMillis());
+        serializer = new OtlpMetricsSerializer(metrics.records(), attributes, context::resolveMetric, resolveKind);
+        lastAttempt = 0;
+        lastSuccess = System.currentTimeMillis();
+        warning = false;
     }
 
     @Override
     public int export()
     {
-        return 0;
+        long now = System.currentTimeMillis();
+        if (now - lastSuccess < interval)
+        {
+            return 0;
+        }
+        if (now - lastAttempt < RETRY_INTERVAL)
+        {
+            return 0;
+        }
+        if (now - lastSuccess >= WARNING_INTERVAL && !warning)
+        {
+            // log warning
+            warning = true;
+        }
+        post();
+        return 1;
+    }
+
+    private void post()
+    {
+        String json = serializer.serializeAll();
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(url)
+            .header("Content-Type", "application/json")
+            .POST(HttpRequest.BodyPublishers.ofString(json))
+            .build();
+        lastAttempt = System.currentTimeMillis();
+        System.out.println(request);
+        CompletableFuture<HttpResponse<String>> response = httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+        response.thenAccept(this::handleResponse);
+    }
+
+    private void handleResponse(
+        HttpResponse<String> response)
+    {
+        int statusCode = response.statusCode();
+        if (statusCode == HttpURLConnection.HTTP_OK)
+        {
+            lastSuccess = System.currentTimeMillis();
+            warning = false;
+        }
     }
 
     @Override
     public void stop()
     {
-        timer.cancel();
     }
 }
