d298fe5b0a08dc8095dbba983894dc7025db840d
Attila Kreiner <kreiner.attila@gmail.com>
2023-08-18 09:29:16 +0200

    fix indent

--- incubator/command-config/src/main/java/io/aklivity/zilla/runtime/command/config/internal/asyncapi/http/proxy/AsyncApiHttpProxyConfigGenerator.java
+++ incubator/command-config/src/main/java/io/aklivity/zilla/runtime/command/config/internal/asyncapi/http/proxy/AsyncApiHttpProxyConfigGenerator.java
@@ -415,27 +415,27 @@ public class AsyncApiHttpProxyConfigGenerator implements ConfigGenerator
         {
             namespace
                 .vault()
-                .name("client")
-                .type("filesystem")
-                .options(FileSystemOptionsConfig::builder)
-                    .trust()
-                        .store("") // env
-                        .type("") // env
-                        .password("") // env
+                    .name("client")
+                    .type("filesystem")
+                    .options(FileSystemOptionsConfig::builder)
+                        .trust()
+                            .store("") // env
+                            .type("") // env
+                            .password("") // env
+                            .build()
                         .build()
                     .build()
-                .build()
-            .vault()
-                .name("server")
-                .type("filesystem")
-                .options(FileSystemOptionsConfig::builder)
-                    .keys()
-                        .store("") // env
-                        .type("") // env
-                        .password("") //env
+                .vault()
+                    .name("server")
+                    .type("filesystem")
+                    .options(FileSystemOptionsConfig::builder)
+                        .keys()
+                            .store("") // env
+                            .type("") // env
+                            .password("") //env
+                            .build()
                         .build()
-                    .build()
-                .build();
+                    .build();
         }
         return namespace;
     }
