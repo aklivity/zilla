8d8489992a51789526cceff1db1edd7e92f5bd42
Attila Kreiner <kreiner.attila@gmail.com>
2023-08-30 09:19:06 +0200

    fix unquoteEnvVars

--- incubator/command-config/src/main/java/io/aklivity/zilla/runtime/command/config/internal/asyncapi/mqtt/proxy/AsyncApiMqttProxyConfigGenerator.java
+++ incubator/command-config/src/main/java/io/aklivity/zilla/runtime/command/config/internal/asyncapi/mqtt/proxy/AsyncApiMqttProxyConfigGenerator.java
@@ -23,7 +23,6 @@ import java.io.InputStream;
 import java.net.URI;
 import java.util.List;
 import java.util.Map;
-import java.util.regex.Pattern;
 
 import jakarta.json.Json;
 import jakarta.json.JsonPatch;
@@ -73,7 +72,7 @@ public class AsyncApiMqttProxyConfigGenerator implements ConfigGenerator
         this.isTlsEnabled = mqttsPorts != null;
         ConfigWriter configWriter = new ConfigWriter(null);
         String yaml = configWriter.write(createNamespace(), createEnvVarsPatch());
-        return unquoteEnvVars(yaml);
+        return unquoteEnvVars(yaml, unquotedEnvVars());
     }
 
     private AsyncApi parseAsyncApi(
@@ -330,17 +329,8 @@ public class AsyncApiMqttProxyConfigGenerator implements ConfigGenerator
         return patch.build();
     }
 
-    private String unquoteEnvVars(
-        String yaml)
+    private List<String> unquotedEnvVars()
     {
-        List<String> unquotedEnvVars = List.of("TCP_CLIENT_PORT");
-        for (String envVar : unquotedEnvVars)
-        {
-            yaml = yaml.replaceAll(
-                Pattern.quote(String.format("\"${{env.%s}}\"", envVar)),
-                String.format("\\${{env.%s}}", envVar)
-            );
-        }
-        return yaml;
+        return List.of("TCP_CLIENT_PORT");
     }
 }
