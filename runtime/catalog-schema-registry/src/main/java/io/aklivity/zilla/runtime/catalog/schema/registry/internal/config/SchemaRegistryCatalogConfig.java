a489dafb2454852757d827e12364db1845929752
Ankit <ankitjames25@gmail.com>
2023-08-30 21:55:51 +0530

    Schema registry implementation

---
+++ runtime/catalog-schema-registry/src/main/java/io/aklivity/zilla/runtime/catalog/schema/registry/internal/config/SchemaRegistryCatalogConfig.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2021-2023 Aklivity Inc.
+ *
+ * Aklivity licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package io.aklivity.zilla.runtime.catalog.schema.registry.internal.config;
+
+import java.util.function.Function;
+
+import io.aklivity.zilla.runtime.engine.config.OptionsConfig;
+
+public class SchemaRegistryCatalogConfig extends OptionsConfig
+{
+    public final String host;
+    public final String port;
+    public final String context;
+
+    public static SchemaRegistryCatalogConfigBuilder<SchemaRegistryCatalogConfig> builder()
+    {
+        return new SchemaRegistryCatalogConfigBuilder<>(SchemaRegistryCatalogConfig.class::cast);
+    }
+
+    public static <T> SchemaRegistryCatalogConfigBuilder<T> builder(
+            Function<OptionsConfig, T> mapper)
+    {
+        return new SchemaRegistryCatalogConfigBuilder<>(mapper);
+    }
+
+    public SchemaRegistryCatalogConfig(
+        String host,
+        String port,
+        String context)
+    {
+        this.host = host;
+        this.port = port;
+        this.context = context;
+    }
+}
