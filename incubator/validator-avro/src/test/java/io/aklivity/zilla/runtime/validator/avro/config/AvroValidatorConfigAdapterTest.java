e7bb2e84e62fbd082693a06c3c31a0eb40d65d40
Ankit <ankitjames25@gmail.com>
2023-09-25 14:25:39 -0700

    unit test fix

--- incubator/validator-avro/src/test/java/io/aklivity/zilla/runtime/validator/avro/config/AvroValidatorConfigAdapterTest.java
+++ incubator/validator-avro/src/test/java/io/aklivity/zilla/runtime/validator/avro/config/AvroValidatorConfigAdapterTest.java
@@ -78,7 +78,7 @@ public class AvroValidatorConfigAdapterTest
         assertThat(validator.catalogs.get(0).schemas.get(0).id, equalTo(0));
         assertThat(validator.catalogs.get(0).schemas.get(1).schema, equalTo("cat"));
         assertThat(validator.catalogs.get(0).schemas.get(1).strategy, nullValue());
-        assertThat(validator.catalogs.get(0).schemas.get(1).version, equalTo("latest"));
+        assertThat(validator.catalogs.get(0).schemas.get(1).version, nullValue());
         assertThat(validator.catalogs.get(0).schemas.get(1).id, equalTo(0));
         assertThat(validator.catalogs.get(0).schemas.get(2).schema, nullValue());
         assertThat(validator.catalogs.get(0).schemas.get(2).strategy, nullValue());
