014b131c11335dc642481acc5070132330e21954
Akram  Yakubov <akram.yakubov@gmail.com>
2023-11-01 18:47:10 -0700

    Remove redandent test

--- specs/binding-kafka.spec/src/test/java/io/aklivity/zilla/specs/binding/kafka/streams/network/GroupIT.java
+++ specs/binding-kafka.spec/src/test/java/io/aklivity/zilla/specs/binding/kafka/streams/network/GroupIT.java
@@ -74,15 +74,6 @@ public class GroupIT
 
     @Test
     @Specification({
-        "${net}/client.sent.write.close.after.sync.group.response/client",
-        "${net}/client.sent.write.close.after.sync.group.response/server"})
-    public void shouldHandleClientSentWriteClosetAfterSyncGroupResponse() throws Exception
-    {
-        k3po.finish();
-    }
-
-    @Test
-    @Specification({
         "${net}/coordinator.not.available/client",
         "${net}/coordinator.not.available/server"})
     public void shouldHandleCoordinatorNotAvailableError() throws Exception
