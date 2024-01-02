ee6ae5f09bc8a4e72852f39c79b2a379a14504ab
Ankit Kumar <ankitjames25@gmail.com>
2024-01-02 23:43:19 +0530

    Protobuf Validation & Conversion

---
+++ incubator/validator-protobuf/src/main/java/io/aklivity/zilla/runtime/validator/protobuf/ProtobufWriteValidator.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2021-2023 Aklivity Inc
+ *
+ * Licensed under the Aklivity Community License (the "License"); you may not use
+ * this file except in compliance with the License.  You may obtain a copy of the
+ * License at
+ *
+ *   https://www.aklivity.io/aklivity-community-license/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package io.aklivity.zilla.runtime.validator.protobuf;
+
+import java.util.function.LongFunction;
+
+import org.agrona.DirectBuffer;
+
+import io.aklivity.zilla.runtime.engine.catalog.CatalogHandler;
+import io.aklivity.zilla.runtime.engine.validator.FragmentValidator;
+import io.aklivity.zilla.runtime.engine.validator.ValueValidator;
+import io.aklivity.zilla.runtime.engine.validator.function.FragmentConsumer;
+import io.aklivity.zilla.runtime.engine.validator.function.ValueConsumer;
+import io.aklivity.zilla.runtime.validator.protobuf.config.ProtobufValidatorConfig;
+
+public class ProtobufWriteValidator extends ProtobufValidator implements ValueValidator, FragmentValidator
+{
+    public ProtobufWriteValidator(
+        ProtobufValidatorConfig config,
+        LongFunction<CatalogHandler> supplyCatalog)
+    {
+        super(config, supplyCatalog);
+    }
+
+    @Override
+    public int padding(
+        DirectBuffer data,
+        int index,
+        int length)
+    {
+        return handler.encodePadding();
+    }
+
+    @Override
+    public int validate(
+        DirectBuffer data,
+        int index,
+        int length,
+        ValueConsumer next)
+    {
+        return validateComplete(data, index, length, next);
+    }
+
+    @Override
+    public int validate(
+        int flags,
+        DirectBuffer data,
+        int index,
+        int length,
+        FragmentConsumer next)
+    {
+        return (flags & FLAGS_FIN) != 0x00
+            ? validateComplete(data, index, length, (b, i, l) -> next.accept(FLAGS_COMPLETE, b, i, l))
+            : 0;
+    }
+
+    private int validateComplete(
+        DirectBuffer data,
+        int index,
+        int length,
+        ValueConsumer next)
+    {
+        int valLength = -1;
+
+        int schemaId = catalog != null && catalog.id > 0
+                ? catalog.id
+                : handler.resolve(subject, catalog.version);
+
+        if (validate(schemaId, data, index, length))
+        {
+            valLength = handler.encode(schemaId, data, index, length, next, CatalogHandler.Encoder.IDENTITY);
+        }
+        return valLength;
+    }
+}
---
+++ incubator/validator-protobuf/src/test/java/io/aklivity/zilla/runtime/validator/protobuf/ProtobufValidatorTest.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2021-2023 Aklivity Inc
+ *
+ * Licensed under the Aklivity Community License (the "License"); you may not use
+ * this file except in compliance with the License.  You may obtain a copy of the
+ * License at
+ *
+ *   https://www.aklivity.io/aklivity-community-license/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package io.aklivity.zilla.runtime.validator.protobuf;
+
+import static io.aklivity.zilla.runtime.engine.EngineConfiguration.ENGINE_DIRECTORY;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+
+import java.util.Properties;
+import java.util.function.LongFunction;
+
+import org.agrona.DirectBuffer;
+import org.agrona.concurrent.UnsafeBuffer;
+import org.junit.Before;
+import org.junit.Test;
+
+import io.aklivity.zilla.runtime.engine.Configuration;
+import io.aklivity.zilla.runtime.engine.EngineContext;
+import io.aklivity.zilla.runtime.engine.catalog.Catalog;
+import io.aklivity.zilla.runtime.engine.catalog.CatalogContext;
+import io.aklivity.zilla.runtime.engine.catalog.CatalogHandler;
+import io.aklivity.zilla.runtime.engine.config.CatalogConfig;
+import io.aklivity.zilla.runtime.engine.test.internal.catalog.TestCatalog;
+import io.aklivity.zilla.runtime.engine.test.internal.catalog.config.TestCatalogOptionsConfig;
+import io.aklivity.zilla.runtime.engine.validator.function.ValueConsumer;
+import io.aklivity.zilla.runtime.validator.protobuf.config.ProtobufValidatorConfig;
+
+public class ProtobufValidatorTest
+{
+    private static final String SCHEMA = "syntax = \"proto3\";" +
+                                    "package io.confluent.examples.clients.basicavro;" +
+                                    "message SimpleMessage " +
+                                    "{  " +
+                                        "string content = 1;" +
+                                        "string date_time = 2;" +
+                                    "}";
+
+    private final ProtobufValidatorConfig protobufConfig = ProtobufValidatorConfig.builder()
+            .catalog()
+                .name("test0")
+                .schema()
+                    .strategy("topic")
+                    .version("latest")
+                    .subject("test-value")
+                    .build()
+                .build()
+            .build();
+    private CatalogContext context;
+
+    @Before
+    public void init()
+    {
+        Properties properties = new Properties();
+        properties.setProperty(ENGINE_DIRECTORY.name(), "target/zilla-itests");
+        Configuration config = new Configuration(properties);
+        Catalog catalog = new TestCatalog(config);
+        context = catalog.supply(mock(EngineContext.class));
+    }
+
+    @Test
+    public void shouldWriteValidProtobufEvent()
+    {
+        CatalogConfig catalogConfig = new CatalogConfig("test0", "test",
+            TestCatalogOptionsConfig.builder()
+                .id(1)
+                .schema(SCHEMA)
+                .build());
+        LongFunction<CatalogHandler> handler = value -> context.attach(catalogConfig);
+        ProtobufWriteValidator validator = new ProtobufWriteValidator(protobufConfig, handler);
+
+        DirectBuffer data = new UnsafeBuffer();
+
+        byte[] bytes = {0x06, 0x69, 0x64, 0x30, 0x10, 0x70, 0x6f,
+            0x73, 0x69, 0x74, 0x69, 0x76, 0x65};
+        data.wrap(bytes, 0, bytes.length);
+
+        byte[] expectedBytes = {0x00, 0x00, 0x00, 0x00, 0x01, 0x06, 0x69, 0x64,
+            0x30, 0x10, 0x70, 0x6f, 0x73, 0x69, 0x74, 0x69, 0x76, 0x65};
+        DirectBuffer expected = new UnsafeBuffer();
+        expected.wrap(expectedBytes);
+        assertEquals(expected.capacity(), validator.validate(data, 0, data.capacity(), ValueConsumer.NOP));
+    }
+}
