924a983fa0e48618fd0023318b78200231434f69
John Fallows <john.r.fallows@gmail.com>
2024-02-04 14:28:10 -0800

    Fix checkstyle whitespace issue

--- runtime/binding-kafka/src/main/java/io/aklivity/zilla/runtime/binding/kafka/identity/KafkaClientIdSupplier.java
+++ runtime/binding-kafka/src/main/java/io/aklivity/zilla/runtime/binding/kafka/identity/KafkaClientIdSupplier.java
@@ -42,7 +42,7 @@ public final class KafkaClientIdSupplier
         String clientId = null;
 
         match:
-        for (int index = 0; index < suppliers.size(); index ++)
+        for (int index = 0; index < suppliers.size(); index++)
         {
             KafkaClientIdSupplierSpi supplier = suppliers.get(index);
             if (supplier.matches(server))
