0d4ce3d1a4da97f6be2af52df8488668b68f4247
Akram  Yakubov <akram.yakubov@gmail.com>
2024-02-03 21:25:00 -0800

    WIP

--- incubator/binding-openapi/src/main/java/io/aklivity/zilla/runtime/binding/openapi/internal/OpenapiBindingContext.java
+++ incubator/binding-openapi/src/main/java/io/aklivity/zilla/runtime/binding/openapi/internal/OpenapiBindingContext.java
@@ -1,17 +1,16 @@
 /*
- * Copyright 2021-2023 Aklivity Inc.
+ * Copyright 2021-2023 Aklivity Inc
  *
- * Aklivity licenses this file to you under the Apache License,
- * version 2.0 (the "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at:
+ * Licensed under the Aklivity Community License (the "License"); you may not use
+ * this file except in compliance with the License.  You may obtain a copy of the
+ * License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ *   https://www.aklivity.io/aklivity-community-license/
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations
- * under the License.
+ * WARRANTIES OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 package io.aklivity.zilla.runtime.binding.openapi.internal;
 
@@ -20,6 +19,7 @@ import static io.aklivity.zilla.runtime.engine.config.KindConfig.SERVER;
 import java.util.EnumMap;
 import java.util.Map;
 
+import io.aklivity.zilla.runtime.binding.openapi.internal.stream.OpenapiServerFactory;
 import io.aklivity.zilla.runtime.binding.openapi.internal.stream.OpenapiStreamFactory;
 import io.aklivity.zilla.runtime.engine.EngineContext;
 import io.aklivity.zilla.runtime.engine.binding.BindingContext;
