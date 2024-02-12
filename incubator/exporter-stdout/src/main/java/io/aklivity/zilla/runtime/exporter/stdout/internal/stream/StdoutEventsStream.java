b934025f360dc61f239bb0c866da9c40de93f048
Attila Kreiner <kreiner.attila@gmail.com>
2024-02-12 15:50:07 +0100

    WIP StdoutEventsStream

--- incubator/exporter-stdout/src/main/java/io/aklivity/zilla/runtime/exporter/stdout/internal/stream/StdoutEventsStream.java
+++ incubator/exporter-stdout/src/main/java/io/aklivity/zilla/runtime/exporter/stdout/internal/stream/StdoutEventsStream.java
@@ -25,18 +25,40 @@ import org.agrona.collections.Int2ObjectHashMap;
 import io.aklivity.zilla.runtime.engine.binding.function.MessageConsumer;
 import io.aklivity.zilla.runtime.engine.spy.RingBufferSpy;
 import io.aklivity.zilla.runtime.exporter.stdout.internal.types.StringFW;
+import io.aklivity.zilla.runtime.exporter.stdout.internal.types.event.EventFW;
+import io.aklivity.zilla.runtime.exporter.stdout.internal.types.event.HttpAuthorizationEventFW;
 import io.aklivity.zilla.runtime.exporter.stdout.internal.types.event.HttpEventFW;
+import io.aklivity.zilla.runtime.exporter.stdout.internal.types.event.KafkaAuthorizationEventFW;
 import io.aklivity.zilla.runtime.exporter.stdout.internal.types.event.KafkaEventFW;
+import io.aklivity.zilla.runtime.exporter.stdout.internal.types.event.MqttAuthorizationEventFW;
 import io.aklivity.zilla.runtime.exporter.stdout.internal.types.event.MqttEventFW;
+import io.aklivity.zilla.runtime.exporter.stdout.internal.types.event.Result;
 import io.aklivity.zilla.runtime.exporter.stdout.internal.types.event.SchemaRegistryEventFW;
+import io.aklivity.zilla.runtime.exporter.stdout.internal.types.event.SchemaRegistryRemoteAccessRejectedEventFW;
 import io.aklivity.zilla.runtime.exporter.stdout.internal.types.event.TcpEventFW;
 import io.aklivity.zilla.runtime.exporter.stdout.internal.types.event.TcpRemoteAccessFailedEventFW;
 import io.aklivity.zilla.runtime.exporter.stdout.internal.types.event.TlsEventFW;
+import io.aklivity.zilla.runtime.exporter.stdout.internal.types.event.TlsFailedEventFW;
 
 public class StdoutEventsStream
 {
+    private static final String HTTP_AUTHORIZATION_FORMAT =
+        "%s: HTTP Authorization %s [timestamp = %d] [traceId = 0x%016x] [binding = %s.%s] [identity = %s]%n";
+    private static final String HTTP_ACCESS_DENIED =
+        "WARNING: HTTP Access Denied [timestamp = %d] [traceId = 0x%016x] [binding = %s.%s]%n";
+    private static final String KAFKA_AUTHORIZATION_FORMAT =
+        "%s: Kafka Authorization %s [timestamp = %d] [traceId = 0x%016x] [binding = %s.%s]%n";
+    private static final String KAFKA_API_VERSION_REJECTED =
+        "ERROR: Kafka API Version Rejected [timestamp = %d] [traceId = 0x%016x] [binding = %s.%s]%n";
+    private static final String MQTT_AUTHORIZATION_FORMAT =
+        "%s: MQTT Authorization %s [timestamp = %d] [traceId = 0x%016x] [binding = %s.%s] [identity = %s]%n";
+    private static final String SCHEMA_REGISTRY_REMOTE_ACCESS_REJECTED =
+        "ERROR: Schema Registry Remote Access Rejected [timestamp = %d] [traceId = 0x%016x] [binding = %s.%s] [url = %s]" +
+        "[method = %s] [status = %d]%n";
     private static final String TCP_REMOTE_ACCESS_FAILED_FORMAT =
-        "ERROR: Remote Access Failed [timestamp = %d] [traceId = 0x%016x] [binding = %s.%s] [address = %s]%n";
+        "ERROR: TCP Remote Access Failed [timestamp = %d] [traceId = 0x%016x] [binding = %s.%s] [address = %s]%n";
+    private static final String TLS_FAILED_FORMAT =
+        "ERROR: TLS Failed [timestamp = %d] [traceId = 0x%016x] [binding = %s.%s] [error = %s]%n";
 
     private final HttpEventFW httpEventRO = new HttpEventFW();
     private final KafkaEventFW kafkaEventRO = new KafkaEventFW();
@@ -112,7 +134,29 @@ public class StdoutEventsStream
         int index,
         int length)
     {
-        // TODO: Ati
+        final HttpEventFW event = httpEventRO.wrap(buffer, index, index + length);
+        switch (event.kind())
+        {
+        case AUTHORIZATION:
+        {
+            HttpAuthorizationEventFW e = event.authorization();
+            String namespace = supplyNamespace.apply(e.namespacedId());
+            String binding = supplyLocalName.apply(e.namespacedId());
+            String level = e.result().get() == Result.SUCCESS ? "INFO" : "WARNING";
+            String result = e.result().get() == Result.SUCCESS ? "Succeeded" : "Failed";
+            out.printf(HTTP_AUTHORIZATION_FORMAT, level, result, e.timestamp(), e.traceId(), namespace, binding,
+                asString(e.identity()));
+            break;
+        }
+        case ACCESS_DENIED:
+        {
+            EventFW e = event.accessDenied();
+            String namespace = supplyNamespace.apply(e.namespacedId());
+            String binding = supplyLocalName.apply(e.namespacedId());
+            out.printf(HTTP_ACCESS_DENIED, e.timestamp(), e.traceId(), namespace, binding);
+            break;
+        }
+        }
     }
 
     private void handleKafkaEvent(
@@ -121,7 +165,28 @@ public class StdoutEventsStream
         int index,
         int length)
     {
-        // TODO: Ati
+        final KafkaEventFW event = kafkaEventRO.wrap(buffer, index, index + length);
+        switch (event.kind())
+        {
+        case AUTHORIZATION:
+        {
+            KafkaAuthorizationEventFW e = event.authorization();
+            String namespace = supplyNamespace.apply(e.namespacedId());
+            String binding = supplyLocalName.apply(e.namespacedId());
+            String level = e.result().get() == Result.SUCCESS ? "INFO" : "WARNING";
+            String result = e.result().get() == Result.SUCCESS ? "Succeeded" : "Failed";
+            out.printf(KAFKA_AUTHORIZATION_FORMAT, level, result, e.timestamp(), e.traceId(), namespace, binding);
+            break;
+        }
+        case API_VERSION_REJECTED:
+        {
+            EventFW e = event.apiVersionRejected();
+            String namespace = supplyNamespace.apply(e.namespacedId());
+            String binding = supplyLocalName.apply(e.namespacedId());
+            out.printf(KAFKA_API_VERSION_REJECTED, e.timestamp(), e.traceId(), namespace, binding);
+            break;
+        }
+        }
     }
 
     private void handleMqttEvent(
@@ -130,7 +195,19 @@ public class StdoutEventsStream
         int index,
         int length)
     {
-        // TODO: Ati
+        MqttEventFW event = mqttEventRO.wrap(buffer, index, index + length);
+        switch (event.kind())
+        {
+        case AUTHORIZATION:
+            MqttAuthorizationEventFW e = event.authorization();
+            String namespace = supplyNamespace.apply(e.namespacedId());
+            String binding = supplyLocalName.apply(e.namespacedId());
+            String level = e.result().get() == Result.SUCCESS ? "INFO" : "WARNING";
+            String result = e.result().get() == Result.SUCCESS ? "Succeeded" : "Failed";
+            out.printf(MQTT_AUTHORIZATION_FORMAT, level, result, e.timestamp(), e.traceId(), namespace, binding,
+                asString(e.identity()));
+            break;
+        }
     }
 
     private void handleSchemaRegistryEvent(
@@ -139,7 +216,17 @@ public class StdoutEventsStream
         int index,
         int length)
     {
-        // TODO: Ati
+        SchemaRegistryEventFW event = schemaRegistryEventRO.wrap(buffer, index, index + length);
+        switch (event.kind())
+        {
+        case REMOTE_ACCESS_REJECTED:
+            SchemaRegistryRemoteAccessRejectedEventFW e = event.remoteAccessRejected();
+            String namespace = supplyNamespace.apply(e.namespacedId());
+            String binding = supplyLocalName.apply(e.namespacedId());
+            out.printf(SCHEMA_REGISTRY_REMOTE_ACCESS_REJECTED, e.timestamp(), e.traceId(), namespace, binding, asString(e.url()),
+                asString(e.method()), e.status());
+            break;
+        }
     }
 
     private void handleTcpEvent(
@@ -166,7 +253,16 @@ public class StdoutEventsStream
         int index,
         int length)
     {
-        // TODO: Ati
+        TlsEventFW event = tlsEventRO.wrap(buffer, index, index + length);
+        switch (event.kind())
+        {
+        case TLS_FAILED:
+            TlsFailedEventFW e = event.tlsFailed();
+            String namespace = supplyNamespace.apply(e.namespacedId());
+            String binding = supplyLocalName.apply(e.namespacedId());
+            out.printf(TLS_FAILED_FORMAT, e.timestamp(), e.traceId(), namespace, binding, e.error().get().name());
+            break;
+        }
     }
 
     private static String asString(
