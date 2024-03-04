52d05d11c6688de966cd522eae726a27f028cc1c
bmaidics <b.maidics@gmail.com>
2024-03-04 13:05:49 +0100

    Increase coverage

--- incubator/catalog-apicurio/src/test/java/io/aklivity/zilla/runtime/catalog/apicurio/internal/ApicurioIT.java
+++ incubator/catalog-apicurio/src/test/java/io/aklivity/zilla/runtime/catalog/apicurio/internal/ApicurioIT.java
@@ -24,6 +24,8 @@ import static org.mockito.Mockito.mock;
 
 import java.time.Duration;
 
+import org.agrona.DirectBuffer;
+import org.agrona.concurrent.UnsafeBuffer;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -35,6 +37,8 @@ import org.kaazing.k3po.junit.rules.K3poRule;
 
 import io.aklivity.zilla.runtime.catalog.apicurio.internal.config.ApicurioOptionsConfig;
 import io.aklivity.zilla.runtime.engine.EngineContext;
+import io.aklivity.zilla.runtime.engine.catalog.CatalogHandler;
+import io.aklivity.zilla.runtime.engine.model.function.ValueConsumer;
 
 public class ApicurioIT
 {
@@ -62,7 +66,7 @@ public class ApicurioIT
     @Test
     @Specification({
         "${local}/resolve.artifact.via.global.id" })
-    public void shouldResolveSchemaViaSchemaId() throws Exception
+    public void shouldResolveArtifactViaGlobalId() throws Exception
     {
         String expected = "asyncapi: 3.0.0\n" +
             "info:\n" +
@@ -78,12 +82,12 @@ public class ApicurioIT
 
         ApicurioCatalogHandler catalog = new ApicurioCatalogHandler(config, context, 0L);
 
-        String schema = catalog.resolve(1);
+        String artifact = catalog.resolve(1);
 
         k3po.finish();
 
-        assertThat(schema, not(nullValue()));
-        assertEquals(expected, schema);
+        assertThat(artifact, not(nullValue()));
+        assertEquals(expected, artifact);
     }
 
     @Test
@@ -107,12 +111,129 @@ public class ApicurioIT
 
         int globalId = catalog.resolve("artifactId", "0");
 
-        String schema = catalog.resolve(globalId);
+        String artifact = catalog.resolve(globalId);
 
         k3po.finish();
 
         assertEquals(globalId, 1);
-        assertThat(schema, not(nullValue()));
-        assertEquals(expected, schema);
+        assertThat(artifact, not(nullValue()));
+        assertEquals(expected, artifact);
+    }
+
+    @Test
+    @Specification({
+        "${local}/resolve.artifact.via.global.id" })
+    public void shouldResolveArtifactViaGlobalIdFromCache() throws Exception
+    {
+        String expected = "asyncapi: 3.0.0\n" +
+            "info:\n" +
+            "  title: Zilla MQTT Proxy\n" +
+            "  version: 1.0.0\n" +
+            "  license:\n" +
+            "    name: Aklivity Community License\n" +
+            "servers:\n" +
+            "  plain:\n" +
+            "    host: mqtt://localhost:7183\n" +
+            "    protocol: mqtt\n" +
+            "defaultContentType: application/json";
+
+        ApicurioCatalogHandler catalog = new ApicurioCatalogHandler(config, context, 0L);
+
+        catalog.resolve(1);
+
+        k3po.finish();
+
+        String artifact = catalog.resolve(1);
+
+        assertThat(artifact, not(nullValue()));
+        assertEquals(expected, artifact);
+    }
+
+    @Test
+    @Specification({
+        "${local}/resolve.artifact.via.artifactid.version" })
+    public void shouldResolveArtifactViaArtifactIdVersionFromCache() throws Exception
+    {
+        String expected = "asyncapi: 3.0.0\n" +
+            "info:\n" +
+            "  title: Zilla MQTT Proxy\n" +
+            "  version: 1.0.0\n" +
+            "  license:\n" +
+            "    name: Aklivity Community License\n" +
+            "servers:\n" +
+            "  plain:\n" +
+            "    host: mqtt://localhost:7183\n" +
+            "    protocol: mqtt\n" +
+            "defaultContentType: application/json";
+
+        ApicurioCatalogHandler catalog = new ApicurioCatalogHandler(config, context, 0L);
+
+        catalog.resolve(catalog.resolve("artifactId", "0"));
+
+        k3po.finish();
+
+        int globalId = catalog.resolve("artifactId", "0");
+
+        String artifact = catalog.resolve(globalId);
+
+        assertEquals(1, globalId);
+        assertThat(artifact, not(nullValue()));
+        assertEquals(expected, artifact);
+    }
+
+    @Test
+    public void shouldVerifyMaxPadding()
+    {
+        ApicurioCatalogHandler catalog = new ApicurioCatalogHandler(config, context, 0L);
+
+        assertEquals(5, catalog.encodePadding());
+    }
+
+    @Test
+    public void shouldVerifyEncodedData()
+    {
+        ApicurioCatalogHandler catalog = new ApicurioCatalogHandler(config, context, 0L);
+
+        DirectBuffer data = new UnsafeBuffer();
+
+        byte[] bytes = {0x06, 0x69, 0x64,
+            0x30, 0x10, 0x70, 0x6f, 0x73, 0x69, 0x74, 0x69, 0x76, 0x65};
+        data.wrap(bytes, 0, bytes.length);
+
+        assertEquals(18, catalog.encode(1, data, 0, data.capacity(),
+            ValueConsumer.NOP, CatalogHandler.Encoder.IDENTITY));
+    }
+
+    @Test
+    public void shouldResolveSchemaIdAndProcessData()
+    {
+
+        ApicurioCatalogHandler catalog = new ApicurioCatalogHandler(config, context, 0L);
+
+        DirectBuffer data = new UnsafeBuffer();
+
+        byte[] bytes = {0x01, 0x00, 0x00, 0x00, 0x09, 0x06, 0x69, 0x64,
+            0x30, 0x10, 0x70, 0x6f, 0x73, 0x69, 0x74, 0x69, 0x76, 0x65};
+        data.wrap(bytes, 0, bytes.length);
+
+        int valLength = catalog.decode(data, 0, data.capacity(), ValueConsumer.NOP, CatalogHandler.Decoder.IDENTITY);
+
+        assertEquals(data.capacity() - 5, valLength);
+    }
+
+    @Test
+    public void shouldResolveSchemaIdFromData()
+    {
+        ApicurioCatalogHandler catalog = new ApicurioCatalogHandler(config, context, 0L);
+
+        DirectBuffer data = new UnsafeBuffer();
+
+        byte[] bytes = {0x01, 0x00, 0x00, 0x00, 0x09, 0x06, 0x69, 0x64,
+            0x30, 0x10, 0x70, 0x6f, 0x73, 0x69, 0x74, 0x69, 0x76, 0x65};
+        data.wrap(bytes, 0, bytes.length);
+
+        int schemaId = catalog.resolve(data, 0, data.capacity());
+
+        assertEquals(9, schemaId);
     }
 }
