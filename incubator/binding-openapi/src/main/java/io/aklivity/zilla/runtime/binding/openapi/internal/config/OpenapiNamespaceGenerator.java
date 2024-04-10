fabb521478b7ae97a25698fdaccb152a613edbb4
Akram  Yakubov <akram.yakubov@gmail.com>
2024-04-10 14:24:10 -0700

    Fix typo

--- incubator/binding-openapi/src/main/java/io/aklivity/zilla/runtime/binding/openapi/internal/config/OpenapiNamespaceGenerator.java
+++ incubator/binding-openapi/src/main/java/io/aklivity/zilla/runtime/binding/openapi/internal/config/OpenapiNamespaceGenerator.java
@@ -33,6 +33,7 @@ import io.aklivity.zilla.runtime.engine.config.NamespaceConfig;
 import io.aklivity.zilla.runtime.engine.config.NamespaceConfigBuilder;
 import io.aklivity.zilla.runtime.engine.config.TelemetryRefConfigBuilder;
 import io.aklivity.zilla.runtime.model.core.config.Int32ModelConfig;
+import io.aklivity.zilla.runtime.model.core.config.Int64ModelConfig;
 import io.aklivity.zilla.runtime.model.core.config.StringModelConfig;
 
 public abstract class OpenapiNamespaceGenerator
@@ -48,7 +49,7 @@ public abstract class OpenapiNamespaceGenerator
         "string", StringModelConfig.builder().build(),
         "integer", Int32ModelConfig.builder().build(),
         "integer:int32", Int32ModelConfig.builder().build(),
-        "integer:int64", Int32ModelConfig.builder().build()
+        "integer:int64", Int64ModelConfig.builder().build()
     );
 
     public abstract NamespaceConfig generate(
