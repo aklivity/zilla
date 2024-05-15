2864bb9878cfdd3d20342e44083d2fb47fcbb087
Ankit Kumar <ankitjames25@gmail.com>
2024-05-15 12:27:18 +0530

    catalog:apicurio - unify caching across workers to maximize cache hits

--- runtime/catalog-apicurio/src/main/java/io/aklivity/zilla/runtime/catalog/apicurio/internal/ApicurioEventContext.java
+++ runtime/catalog-apicurio/src/main/java/io/aklivity/zilla/runtime/catalog/apicurio/internal/ApicurioEventContext.java
@@ -14,9 +14,12 @@
  */
 package io.aklivity.zilla.runtime.catalog.apicurio.internal;
 
-import static io.aklivity.zilla.runtime.catalog.apicurio.internal.types.event.ApicurioEventType.REMOTE_ACCESS_REJECTED;
+import static io.aklivity.zilla.runtime.catalog.apicurio.internal.types.event.ApicurioEventType.RETRIEVED_ARTIFACT_ID;
+import static io.aklivity.zilla.runtime.catalog.apicurio.internal.types.event.ApicurioEventType.RETRIEVED_ARTIFACT_SUBJECT_VERSION;
+import static io.aklivity.zilla.runtime.catalog.apicurio.internal.types.event.ApicurioEventType.UNRETRIEVABLE_ARTIFACT_ID;
+import static io.aklivity.zilla.runtime.catalog.apicurio.internal.types.event.ApicurioEventType.UNRETRIEVABLE_ARTIFACT_SUBJECT_VERSION;
+import static io.aklivity.zilla.runtime.catalog.apicurio.internal.types.event.ApicurioEventType.UNRETRIEVABLE_ARTIFACT_SUBJECT_VERSION_STALE_ARTIFACT;
 
-import java.net.http.HttpRequest;
 import java.nio.ByteBuffer;
 import java.time.Clock;
 
@@ -35,9 +38,13 @@ public class ApicurioEventContext
     private final MutableDirectBuffer eventBuffer = new UnsafeBuffer(ByteBuffer.allocate(EVENT_BUFFER_CAPACITY));
     private final MutableDirectBuffer extensionBuffer = new UnsafeBuffer(ByteBuffer.allocate(EVENT_BUFFER_CAPACITY));
     private final EventFW.Builder eventRW = new EventFW.Builder();
-    private final ApicurioEventExFW.Builder schemaRegistryEventExRW = new ApicurioEventExFW.Builder();
+    private final ApicurioEventExFW.Builder apicurioEventExRW = new ApicurioEventExFW.Builder();
     private final int apicurioTypeId;
-    private final int remoteAccessRejectedEventId;
+    private final int unretrievableArtifactSubjectVersionId;
+    private final int staleArtifactID;
+    private final int unretrievableArtifactId;
+    private final int retrievableArtifactSubjectVersionId;
+    private final int retrievableArtifactId;
     private final MessageConsumer eventWriter;
     private final Clock clock;
 
@@ -45,28 +52,127 @@ public class ApicurioEventContext
         EngineContext context)
     {
         this.apicurioTypeId = context.supplyTypeId(ApicurioCatalog.NAME);
-        this.remoteAccessRejectedEventId = context.supplyEventId("catalog.apicurio.remote.access.rejected");
+        this.unretrievableArtifactSubjectVersionId = context.supplyEventId(
+            "catalog.apicurio.unretrievable.artifact.subject.version");
+        this.staleArtifactID = context.supplyEventId("catalog.apicurio.unretrievable.artifact.subject.version.stale.artifact");
+        this.unretrievableArtifactId = context.supplyEventId("catalog.apicurio.unretrievable.artifact.id");
+        this.retrievableArtifactSubjectVersionId = context.supplyEventId(
+            "catalog.apicurio.retrievable.artifact.subject.version");
+        this.retrievableArtifactId = context.supplyEventId("catalog.apicurio.retrievable.artifact.id");
         this.eventWriter = context.supplyEventWriter();
         this.clock = context.clock();
     }
 
-    public void remoteAccessRejected(
+    public void onUnretrievableArtifactSubjectVersion(
         long catalogId,
-        HttpRequest httpRequest,
-        int status)
+        String subject,
+        String version)
     {
-        ApicurioEventExFW extension = schemaRegistryEventExRW
+        ApicurioEventExFW extension = apicurioEventExRW
             .wrap(extensionBuffer, 0, extensionBuffer.capacity())
-            .remoteAccessRejected(e -> e
-                .typeId(REMOTE_ACCESS_REJECTED.value())
-                .method(httpRequest.method())
-                .url(httpRequest.uri().toString())
-                .status((short) status)
+            .unretrievableArtifactSubjectVersion(e -> e
+                .typeId(UNRETRIEVABLE_ARTIFACT_SUBJECT_VERSION.value())
+                .subject(subject)
+                .version(version)
             )
             .build();
         EventFW event = eventRW
             .wrap(eventBuffer, 0, eventBuffer.capacity())
-            .id(remoteAccessRejectedEventId)
+            .id(unretrievableArtifactSubjectVersionId)
+            .timestamp(clock.millis())
+            .traceId(0L)
+            .namespacedId(catalogId)
+            .extension(extension.buffer(), extension.offset(), extension.limit())
+            .build();
+        eventWriter.accept(apicurioTypeId, event.buffer(), event.offset(), event.limit());
+    }
+
+    public void onUnretrievableArtifactSubjectVersionStaleArtifact(
+        long catalogId,
+        String subject,
+        String version,
+        int artifactId)
+    {
+        ApicurioEventExFW extension = apicurioEventExRW
+            .wrap(extensionBuffer, 0, extensionBuffer.capacity())
+            .unretrievableArtifactSubjectVersionStaleArtifact(e -> e
+                .typeId(UNRETRIEVABLE_ARTIFACT_SUBJECT_VERSION_STALE_ARTIFACT.value())
+                .subject(subject)
+                .version(version)
+                .artifactId(artifactId)
+            )
+            .build();
+        EventFW event = eventRW
+            .wrap(eventBuffer, 0, eventBuffer.capacity())
+            .id(staleArtifactID)
+            .timestamp(clock.millis())
+            .traceId(0L)
+            .namespacedId(catalogId)
+            .extension(extension.buffer(), extension.offset(), extension.limit())
+            .build();
+        eventWriter.accept(apicurioTypeId, event.buffer(), event.offset(), event.limit());
+    }
+
+    public void onUnretrievableArtifactId(
+        long catalogId,
+        int artifactId)
+    {
+        ApicurioEventExFW extension = apicurioEventExRW
+            .wrap(extensionBuffer, 0, extensionBuffer.capacity())
+            .unretrievableArtifactId(e -> e
+                .typeId(UNRETRIEVABLE_ARTIFACT_ID.value())
+                .artifactId(artifactId)
+            )
+            .build();
+        EventFW event = eventRW
+            .wrap(eventBuffer, 0, eventBuffer.capacity())
+            .id(unretrievableArtifactId)
+            .timestamp(clock.millis())
+            .traceId(0L)
+            .namespacedId(catalogId)
+            .extension(extension.buffer(), extension.offset(), extension.limit())
+            .build();
+        eventWriter.accept(apicurioTypeId, event.buffer(), event.offset(), event.limit());
+    }
+
+    public void onRetrievableArtifactSubjectVersion(
+        long catalogId,
+        String subject,
+        String version)
+    {
+        ApicurioEventExFW extension = apicurioEventExRW
+            .wrap(extensionBuffer, 0, extensionBuffer.capacity())
+            .retrievableArtifactSubjectVersion(e -> e
+                .typeId(RETRIEVED_ARTIFACT_SUBJECT_VERSION.value())
+                .subject(subject)
+                .version(version)
+            )
+            .build();
+        EventFW event = eventRW
+            .wrap(eventBuffer, 0, eventBuffer.capacity())
+            .id(retrievableArtifactSubjectVersionId)
+            .timestamp(clock.millis())
+            .traceId(0L)
+            .namespacedId(catalogId)
+            .extension(extension.buffer(), extension.offset(), extension.limit())
+            .build();
+        eventWriter.accept(apicurioTypeId, event.buffer(), event.offset(), event.limit());
+    }
+
+    public void onRetrievableArtifactId(
+        long catalogId,
+        int artifactId)
+    {
+        ApicurioEventExFW extension = apicurioEventExRW
+            .wrap(extensionBuffer, 0, extensionBuffer.capacity())
+            .retrievableArtifactId(e -> e
+                .typeId(RETRIEVED_ARTIFACT_ID.value())
+                .artifactId(artifactId)
+            )
+            .build();
+        EventFW event = eventRW
+            .wrap(eventBuffer, 0, eventBuffer.capacity())
+            .id(retrievableArtifactId)
             .timestamp(clock.millis())
             .traceId(0L)
             .namespacedId(catalogId)
