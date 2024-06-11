edaf55d98ff845e7d49b7c7c7f8cb8d355530d16
John Fallows <john.r.fallows@gmail.com>
2024-06-10 20:47:35 -0700

    Simplify assert for tcp reset

--- runtime/binding-tcp/src/main/java/io/aklivity/zilla/runtime/binding/tcp/internal/stream/TcpServerFactory.java
+++ runtime/binding-tcp/src/main/java/io/aklivity/zilla/runtime/binding/tcp/internal/stream/TcpServerFactory.java
@@ -582,7 +582,6 @@ public class TcpServerFactory implements TcpStreamFactory
             final long traceId = reset.traceId();
 
             assert acknowledge <= sequence;
-            assert sequence <= initialSeq;
             assert acknowledge >= initialAck;
 
             initialAck = acknowledge;
