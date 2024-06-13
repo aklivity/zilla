3fad35da38dfce03cb1a5ce6c186422f8596fb7e
Attila Kreiner <kreiner.attila@gmail.com>
2024-06-13 18:41:01 +0200

    Fix TlsNetworkIT by adding cipherSuites (#1043)

--- incubator/command-dump/src/test/java/io/aklivity/zilla/runtime/command/dump/internal/TlsNetworkIT.java
+++ incubator/command-dump/src/test/java/io/aklivity/zilla/runtime/command/dump/internal/TlsNetworkIT.java
@@ -17,7 +17,6 @@ package io.aklivity.zilla.runtime.command.dump.internal;
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.junit.rules.RuleChain.outerRule;
 
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.DisableOnDebug;
@@ -42,7 +41,6 @@ public class TlsNetworkIT
     @Rule
     public final TestRule chain = outerRule(dump).around(k3po).around(timeout);
 
-    @Ignore
     @Test
     @Specification({
         "${net}/connection.established/client",
