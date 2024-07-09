8c2a8212f71254b1cc388f92b53025b9e6add8a1
John Fallows <john.r.fallows@gmail.com>
2024-07-08 21:34:30 -0700

    Use karapace-schema-registry as canonical name and verify karapace alias still works

--- specs/catalog-karapace.spec/src/test/java/io/aklivity/zilla/specs/catalog/karapace/config/SchemaTest.java
+++ specs/catalog-karapace.spec/src/test/java/io/aklivity/zilla/specs/catalog/karapace/config/SchemaTest.java
@@ -40,4 +40,12 @@ public class SchemaTest
 
         assertThat(config, not(nullValue()));
     }
+
+    @Test
+    public void shouldValidateCatalogWithAlias()
+    {
+        JsonObject config = schema.validate("catalog-alias.yaml");
+
+        assertThat(config, not(nullValue()));
+    }
 }
