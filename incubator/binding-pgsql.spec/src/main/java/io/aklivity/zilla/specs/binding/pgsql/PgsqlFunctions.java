83eb9ed865bb380ba4366aa4d0da5b382a89ab16
Akram  Yakubov <akram.yakubov@gmail.com>
2024-08-14 17:06:41 -0700

    WIP

---
+++ incubator/binding-pgsql.spec/src/main/java/io/aklivity/zilla/specs/binding/pgsql/PgsqlFunctions.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2021-2023 Aklivity Inc
+ *
+ * Licensed under the Aklivity Community License (the "License"); you may not use
+ * this file except in compliance with the License.  You may obtain a copy of the
+ * License at
+ *
+ *   https://www.aklivity.io/aklivity-community-license/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package io.aklivity.zilla.specs.binding.pgsql;
+
+import org.agrona.MutableDirectBuffer;
+import org.agrona.concurrent.UnsafeBuffer;
+
+import io.aklivity.zilla.specs.binding.pgsql.internal.types.stream.PgsqlDataExFW;
+import io.aklivity.zilla.specs.binding.pgsql.internal.types.stream.PgsqlQueryDataExFW;
+import io.aklivity.zilla.specs.binding.pgsql.internal.types.stream.PgsqlRowDataExFW;
+
+public final class PgsqlFunctions
+{
+    public static final class PgsqlDataExBuilder
+    {
+        private final MutableDirectBuffer writeBuffer = new UnsafeBuffer(new byte[1024 * 8]);
+
+        private final PgsqlDataExFW dataExRO = new PgsqlDataExFW();
+
+        private final PgsqlDataExFW.Builder dataExRW = new PgsqlDataExFW.Builder();
+
+        private PgsqlDataExBuilder()
+        {
+            dataExRW.wrap(writeBuffer, 0, writeBuffer.capacity());
+        }
+
+        public PgsqlDataExBuilder typeId(
+            int typeId)
+        {
+            dataExRW.typeId(typeId);
+            return this;
+        }
+
+        public PgsqlQueryDataExBuilder query()
+        {
+            dataExRW.kind(81);
+
+            return new PgsqlQueryDataExBuilder();
+        }
+
+        public byte[] build()
+        {
+            final PgsqlDataExFW dataEx = dataExRO;
+            final byte[] array = new byte[dataEx.sizeof()];
+            dataEx.buffer().getBytes(dataEx.offset(), array);
+            return array;
+        }
+
+        public final class PgsqlQueryDataExBuilder
+        {
+            private final PgsqlQueryDataExFW.Builder pgsqlQueryDataExRW = new PgsqlQueryDataExFW.Builder();
+
+            private PgsqlQueryDataExBuilder()
+            {
+                pgsqlQueryDataExRW.wrap(writeBuffer, PgsqlDataExFW.FIELD_OFFSET_QUERY, writeBuffer.capacity());
+            }
+
+            public PgsqlDataExBuilder build()
+            {
+                final PgsqlQueryDataExFW pgsqlQueryDataEx = pgsqlQueryDataExRW.build();
+                dataExRO.wrap(writeBuffer, 0, pgsqlQueryDataEx.limit());
+                return PgsqlDataExBuilder.this;
+            }
+        }
+
+        public final class PgsqlRowDataExBuilder
+        {
+            private final PgsqlRowDataExFW.Builder pgsqlRowDataExRW = new PgsqlRowDataExFW.Builder();
+
+            private PgsqlRowDataExBuilder()
+            {
+                pgsqlRowDataExRW.wrap(writeBuffer, PgsqlDataExFW.FIELD_OFFSET_QUERY, writeBuffer.capacity());
+            }
+
+            public PgsqlDataExBuilder build()
+            {
+                final PgsqlRowDataExFW pgsqlQueryDataEx = pgsqlRowDataExRW.build();
+                dataExRO.wrap(writeBuffer, 0, pgsqlQueryDataEx.limit());
+                return PgsqlDataExBuilder.this;
+            }
+        }
+    }
+
+    private PgsqlFunctions()
+    {
+        // utility
+    }
+}
