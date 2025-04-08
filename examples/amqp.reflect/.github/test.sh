88f71330b40a1117a68056f67872eee81fb95b21
Ankit Kumar <ankitjames25@gmail.com>
2025-04-08 20:18:15 +0530

    Add `test.sh` for `amqp.reflect` & skip fix required examples (#1452)

---
+++ examples/amqp.reflect/.github/test.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+set -x
+
+EXIT=0
+
+# GIVEN
+INPUT="Hello, Zilla!"
+EXPECTED="Hello, Zilla!"
+RECEIVER_LOG="output.out"
+
+echo "# Testing amqp.reflect/"
+echo "INPUT=$INPUT"
+echo "EXPECTED=$EXPECTED"
+echo
+
+docker compose -p zilla-amqp-reflect exec cli-rhea cli-rhea-receiver \
+  --address 'zilla' \
+  --count 1 \
+  --log-msgs 'body' \
+  --broker zilla.examples.dev:7172 >"$RECEIVER_LOG" 2>&1 &
+
+RECEIVER_PID=$!
+
+sleep 2
+
+timeout 2 docker compose -p zilla-amqp-reflect exec cli-rhea cli-rhea-sender \
+  --address 'zilla' \
+  --msg-content "$INPUT" \
+  --count 1 \
+  --broker zilla.examples.dev:7172
+RESULT=$?
+
+wait "$RECEIVER_PID"
+
+OUTPUT=$(grep -o "$INPUT" "$RECEIVER_LOG" | head -n 1)
+
+rm -f "$RECEIVER_LOG"
+
+echo "OUTPUT=\"$OUTPUT\""
+echo "EXPECTED=\"$EXPECTED\""
+echo
+
+if [ "$OUTPUT" = "$EXPECTED" ]; then
+  echo "✅"
+else
+  echo "❌"
+  EXIT=1
+fi
+
+exit $EXIT
