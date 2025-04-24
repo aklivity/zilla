58b283b052821143956a576866b0007c01402056
Ankit Kumar <ankitjames25@gmail.com>
2025-04-24 16:38:54 +0530

    added support for validation in asyncapi.sse flow & example fix

--- examples/asyncapi.sse.proxy/.github/test.sh
+++ examples/asyncapi.sse.proxy/.github/test.sh
@@ -7,7 +7,7 @@ EXIT=0
 PORT="7114"
 INPUT_GOOD='{ "name": "event name", "data": { "id": 1, "name": "Hello World!" } }'
 INPUT_BAD='{ "name": "event name", "data": { "id": -1, "name": "Hello bad World!" } }'
-EXPECTED='data:{ "id": 1, "name": "Hello World!" }'
+EXPECTED='data:{"id":1,"name":"Hello World!"}'
 echo \# Testing asyncapi.sse.proxy/
 echo PORT="$PORT"
 echo INPUT_GOOD="$INPUT_GOOD"
@@ -19,13 +19,13 @@ echo
 # send request to zilla
 timeout 3s curl -N --http2 -H "Accept:text/event-stream" "http://localhost:$PORT/events/1" | tee .testoutput &
 
-# push response to kafka with kafkacat
+# push events to sse server
 echo "$INPUT_GOOD" | nc -c localhost 7001
 echo "$INPUT_BAD" | nc -c localhost 7001
 
 # fetch the output of zilla request; try 5 times
 for i in $(seq 0 2); do
-  sleep $i
+  sleep 5
   OUTPUT=$(cat .testoutput | grep "^data:")
   if [ -n "$OUTPUT" ]; then
     break
@@ -43,10 +43,5 @@ else
   echo ❌
   EXIT=1
 fi
-
-# TODO remove once fixed
-echo '❌ curl: (52) Empty reply from server. Tested on main. and does not work with described instructions'
-echo 'Refer: https://github.com/aklivity/zilla/issues/1417'
-EXIT=1
 
 exit $EXIT
