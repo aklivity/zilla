b8e5fe65fec5e08d54467c2440e36fba38249a21
Ankit Kumar <ankitjames25@gmail.com>
2026-04-02 22:42:47 +0530

    module update

--- runtime/engine/src/main/moditect/module-info.java
+++ runtime/engine/src/main/moditect/module-info.java
@@ -17,6 +17,7 @@ module io.aklivity.zilla.runtime.engine
 {
     exports io.aklivity.zilla.runtime.engine;
     exports io.aklivity.zilla.runtime.engine.config;
+    exports io.aklivity.zilla.runtime.engine.diagnostic;
 
     exports io.aklivity.zilla.runtime.engine.binding;
     exports io.aklivity.zilla.runtime.engine.binding.function;
